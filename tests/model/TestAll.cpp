192f453c0e67ea6584da7147242de639cb6a7dee
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-09-28 16:51:10 +0200

    Unused test removed, file renamed.

--- tests/model/TestAll.cpp
+++ tests/model/TestAll.cpp
@@ -1,9 +1,9 @@
 #include <gtest/gtest.h>
 #include "TestSessionItem.h"
-#include "TestAnother.h"
 #include "TestSessionModel.h"
 #include "TestPath.h"
 #include "TestUndoRedo.h"
+#include "TestItemPool.h"
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
