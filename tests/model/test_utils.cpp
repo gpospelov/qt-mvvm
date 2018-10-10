3f5f8637fc34150b353f209a5e04269509604418
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-10-10 10:31:54 +0200

    Common test functions to deal with json files moved to TestUtils

---
+++ tests/model/test_utils.cpp
@@ -0,0 +1,62 @@
+// ************************************************************************** //
+//
+//  Prototype of mini MVVM framework for bornagainproject.org
+//
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher
+//
+// ************************************************************************** //
+
+#include "test_utils.h"
+#include "testconfig.h"
+#include "fileutils.h"
+#include <QFile>
+#include <QString>
+#include <QJsonDocument>
+#include <string>
+
+namespace {
+void SaveDocument(const QJsonDocument& document, const QString& fileName);
+}
+
+void TestUtils::CreateTestDirectory(const QString& test_sub_dir)
+{
+    auto path = TestDirectoryPath(test_sub_dir);
+    Utils::create_dir(path.toStdString());
+}
+
+QString TestUtils::TestDirectoryPath(const QString& test_sub_dir)
+{
+    return QString::fromStdString(Testing::TestOutputDir()) + "/" + test_sub_dir;
+}
+
+QString TestUtils::TestFileName(const QString& test_sub_dir, const QString& file_name)
+{
+    return TestDirectoryPath(test_sub_dir) + "/" + file_name;
+}
+
+void TestUtils::SaveJson(const QJsonObject& object, const QString& fileName)
+{
+    QJsonDocument document(object);
+    SaveDocument(document, fileName);
+}
+
+void TestUtils::SaveJson(const QJsonArray& object, const QString& fileName)
+{
+    QJsonDocument document(object);
+    SaveDocument(document, fileName);
+}
+
+namespace {
+
+void SaveDocument(const QJsonDocument& document, const QString& fileName)
+{
+    QFile saveFile(fileName);
+
+    if (!saveFile.open(QIODevice::WriteOnly))
+        throw std::runtime_error("TestUtils::SaveDocument() -> Can't save file");
+
+    saveFile.write(document.toJson());
+}
+
+}
---
+++ tests/model/test_utils.h
@@ -0,0 +1,38 @@
+// ************************************************************************** //
+//
+//  Prototype of mini MVVM framework for bornagainproject.org
+//
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher
+//
+// ************************************************************************** //
+
+#ifndef TESTUTILS_H
+#define TESTUTILS_H
+
+#include <QString>
+class QJsonObject;
+class QJsonArray;
+
+//! Various common utils for unit tests.
+
+namespace TestUtils
+{
+
+//! Creates test directory in CMAKE_BINARY_DIR. If directory exists, it will be removed
+//! recursively first.
+void CreateTestDirectory(const QString& test_sub_dir);
+
+//! Returns full path to the test directory in CMAKE_BINARY_DIR.
+QString TestDirectoryPath(const QString& test_sub_dir);
+
+//! Returns full path to the file in test directory.
+QString TestFileName(const QString& test_sub_dir, const QString& file_name);
+
+void SaveJson(const QJsonObject& object, const QString& fileName);
+
+void SaveJson(const QJsonArray& object, const QString& fileName);
+
+}
+
+#endif
