786bf592efe1be5bd42cc15a2224ddb6a4b7b4cd
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-10-26 17:27:04 +0200

    DLL_EXPORT flag defined in correct cmake.txt

--- cmake/modules/configuration.cmake
+++ cmake/modules/configuration.cmake
@@ -26,9 +26,3 @@ message(STATUS "  Includes: ${Qt5Widgets_INCLUDE_DIRS}")
 
 configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/testconfig.h.in  ${BUILD_INC_DIR}/testconfig.h @ONLY)
 add_definitions(-I${BUILD_INC_DIR})
-
-# Windows specific
-
-if(WIN32)
-    add_definitions(-DCORE_BUILD_DLL)
-endif()
