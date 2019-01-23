2434426e43670475242b7b8e440c78b2dcd38ec1
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-01-23 16:32:17 +0100

    Placeholders for core gui library

---
+++ app/appcore/mainwindow.cpp
@@ -0,0 +1,15 @@
+// ************************************************************************** //
+//
+//  Prototype of mini MVVM framework for bornagainproject.org
+//
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher
+//
+// ************************************************************************** //
+
+#include "mainwindow.h"
+
+MainWindow::MainWindow()
+{
+
+}
---
+++ app/appcore/mainwindow.h
@@ -0,0 +1,23 @@
+// ************************************************************************** //
+//
+//  Prototype of mini MVVM framework for bornagainproject.org
+//
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher
+//
+// ************************************************************************** //
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "global.h"
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow();
+};
+
+#endif //  MAINWINDOW_H
