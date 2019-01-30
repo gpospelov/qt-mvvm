c9449c97d5324ca0b0442c3d192e43ea3103fd60
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-01-30 16:11:39 +0100

    Allow QSignalSpy usage together with QStandardItem

--- tests/viewmodel/TestAll.cpp
+++ tests/viewmodel/TestAll.cpp
@@ -1,9 +1,11 @@
 #include "google_test.h"
+#include <QStandardItem>
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
 
     ModelView::Comparators::registerComparators();
+    qRegisterMetaType<QStandardItem*>("QStandardItem*");
 
     // run all google tests
     return RUN_ALL_TESTS();
