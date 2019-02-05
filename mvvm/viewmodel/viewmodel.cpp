2f3a546bf94204747123ceaa8b92a6341720b956
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-02-05 16:15:33 +0100

    More accurate ViewModel initialization

--- mvvm/viewmodel/viewmodel.cpp
+++ mvvm/viewmodel/viewmodel.cpp
@@ -8,61 +8,65 @@
 // ************************************************************************** //
 
 #include "viewmodel.h"
-#include "sessionmodel.h"
 #include "itemutils.h"
-#include "sessionitem.h"
-#include "viewitems.h"
 #include "modelmapper.h"
-#include <algorithm>
+#include "sessionitem.h"
+#include "sessionmodel.h"
+#include "viewitems.h"
 #include <QDebug>
+#include <algorithm>
 
 using namespace ModelView;
 
-namespace {
+namespace
+{
 
 //! Constructs row (display name and data) for given item.
-std::vector<ViewItem* > constructRow(SessionItem* item)
+std::vector<ViewItem*> constructRow(SessionItem* item)
 {
-    std::vector<ViewItem* > result;
+    std::vector<ViewItem*> result;
     result.push_back(new ViewLabelItem(item));
     if (item->data(ItemDataRole::DATA).isValid())
         result.push_back(new ViewDataItem(item));
     return result;
 }
 
-QList<QStandardItem*> toStandardItemList(const std::vector<ViewItem* >& items)
+QList<QStandardItem*> toStandardItemList(const std::vector<ViewItem*>& items)
 {
     QList<QStandardItem*> result;
-    std::transform(items.begin(), items.end(), std::back_inserter(result), [](ViewItem* item) { return item; });
+    std::transform(items.begin(), items.end(), std::back_inserter(result),
+                   [](ViewItem* item) { return item; });
     return result;
 }
-
 }
 
-
-ViewModel::ViewModel(QObject* parent)
-    : QStandardItemModel(parent)
-    , m_sessionModel(nullptr)
+ViewModel::ViewModel(QObject* parent) : QStandardItemModel(parent), m_sessionModel(nullptr)
 {
     setItemPrototype(new ViewEmptyItem);
 }
 
 void ViewModel::setSessionModel(SessionModel* model)
 {
+    if (m_sessionModel) {
+        m_sessionModel->mapper()->unsubscribe(this);
+    }
+
     m_sessionModel = model;
 
-    m_sessionModel->mapper()->setOnDataChange([this](ModelView::SessionItem* item, int role) {
-        onDataChange(item, role);
-    }, this);
+    if (m_sessionModel) {
+        m_sessionModel->mapper()->setOnDataChange(
+            [this](ModelView::SessionItem* item, int role) { onDataChange(item, role); }, this);
 
-    m_sessionModel->mapper()->setOnRowInserted([this](ModelView::SessionItem* item, int row) {
-        onRowInserted(item, row);
-    }, this);
+        m_sessionModel->mapper()->setOnRowInserted(
+            [this](ModelView::SessionItem* item, int row) { onRowInserted(item, row); }, this);
 
-    m_sessionModel->mapper()->setOnRowRemoved([this](ModelView::SessionItem* item, int row) {
-        onRowRemoved(item, row);
-    }, this);
+        m_sessionModel->mapper()->setOnRowRemoved(
+            [this](ModelView::SessionItem* item, int row) { onRowRemoved(item, row); }, this);
+    }
 
+    m_item_to_view.clear();
+    clear();
+    setColumnCount(2);
 
     update_model();
 }
@@ -87,11 +91,8 @@ void ViewModel::onRowRemoved(SessionItem* parent, int row)
 
 void ViewModel::update_model()
 {
-    clear();
-    if(!m_sessionModel)
+    if (!m_sessionModel)
         return;
-
-    setColumnCount(2);
 
     iterate(m_sessionModel->rootItem(), invisibleRootItem());
 }
@@ -102,7 +103,7 @@ void ViewModel::iterate(SessionItem* item, QStandardItem* parent)
     int insert_index(0);
     for (auto child : item->children()) {
 
-        std::vector<ViewItem* > row;
+        std::vector<ViewItem*> row;
 
         auto it = m_item_to_view.find(child);
         if (it == m_item_to_view.end()) {
