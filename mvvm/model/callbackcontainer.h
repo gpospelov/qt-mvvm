ef81344485416b4d9a8b905b1c4a0bc2b8f7af62
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-02-20 14:41:10 +0100

    Switch object passing from forward to by value

--- mvvm/model/callbackcontainer.h
+++ mvvm/model/callbackcontainer.h
@@ -12,8 +12,8 @@
 
 #include "global.h"
 #include "model_types.h"
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 namespace ModelView
 {
@@ -23,15 +23,14 @@ class SessionModel;
 
 //! Container to hold callbacks in the context of ModelMapper.
 
-template<typename T, typename U>
-class CallbackBaseContainer
+template <typename T, typename U> class CallbackBaseContainer
 {
 public:
     CallbackBaseContainer() = default;
 
     void add(T callback, U caller);
 
-    template <typename... Args> void notify(Args&&... args);
+    template <typename... Args> void notify(Args... args);
 
     void remove_caller(U caller);
 
@@ -39,41 +38,37 @@ private:
     std::vector<std::pair<T, U>> m_callbacks;
 };
 
-template<typename T, typename U>
-void CallbackBaseContainer<T, U>::add(T callback, U caller)
+template <typename T, typename U> void CallbackBaseContainer<T, U>::add(T callback, U caller)
 {
     m_callbacks.push_back(std::make_pair(callback, caller));
 }
 
 //! Notify clients using given list of arguments.
 
-template<typename T, typename U>
-template<typename... Args>
-void CallbackBaseContainer<T, U>::notify(Args&&... args)
+template <typename T, typename U>
+template <typename... Args>
+void CallbackBaseContainer<T, U>::notify(Args... args)
 {
     for (auto f : m_callbacks)
-        f.first(std::forward<Args>(args)...);
+        f.first(args...);
 }
 
 //! Remove client from the list to call back.
 
-template<typename T, typename U>
-void CallbackBaseContainer<T, U>::remove_caller(U caller)
+template <typename T, typename U> void CallbackBaseContainer<T, U>::remove_caller(U caller)
 {
     m_callbacks.erase(std::remove_if(m_callbacks.begin(), m_callbacks.end(),
-                           [caller](typename std::vector<std::pair<T, U>>::value_type const& x) -> bool {
-                               return (x.second == caller ? true : false);
-                           }),
-            m_callbacks.end());
+                                     [caller](const std::pair<T, U>& x) -> bool {
+                                         return (x.second == caller ? true : false);
+                                     }),
+                      m_callbacks.end());
 }
 
 //! Callback container for specific caller type.
 
-template<typename T>
-class CallbackContainer : public CallbackBaseContainer<T, Callbacks::caller_t>
+template <typename T> class CallbackContainer : public CallbackBaseContainer<T, Callbacks::caller_t>
 {
 };
-
 
 } // ModelView
 
