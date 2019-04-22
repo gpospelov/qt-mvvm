8bd63fdf0f6bd5112ae52a0a12757fc93a8893e3
Dmitry Yurov <d.yurov@fz-juelich.de>
2019-04-23 00:10:26 +0200

    Add spin box to the assembly header

--- examples/LayerEditor/tableviewdelegate.cpp
+++ examples/LayerEditor/tableviewdelegate.cpp
@@ -1,13 +1,42 @@
 #include "tableviewdelegate.h"
 #include "customtableview.h"
+#include "detailsbutton.h"
 #include "detailswidget.h"
 #include "treeitem.h"
 #include <QHeaderView>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QSpinBox>
 
 namespace {
+	class DisabledFadingPanel : public Utils::FadingPanel
+	{
+	public:
+		DisabledFadingPanel(QWidget* parent = nullptr)
+			: Utils::FadingPanel(parent)
+		{}
+		void fadeTo(qreal) override {}
+		void setOpacity(qreal) override {}
+	};
+
+	void setSpinBoxToDetailsWidget(Utils::DetailsWidget* details_widget, const QModelIndex& index)
+	{
+		DisabledFadingPanel* fading_widget = new DisabledFadingPanel(details_widget);
+		auto layout_h = new QHBoxLayout();
+		layout_h->addWidget(new QLabel("Number of repetitions:"));
+
+		auto spinbox = new QSpinBox();
+		spinbox->setValue(1);
+		layout_h->addWidget(spinbox);
+
+		fading_widget->setLayout(layout_h);
+		details_widget->setToolWidget(fading_widget);
+	}
+
 	Utils::DetailsWidget* createDetailsWidget(QWidget* parent, const QModelIndex& index)
 	{
 		auto result = new Utils::DetailsWidget(parent);
+		setSpinBoxToDetailsWidget(result, index);
 		auto internal_table = new CustomTableView();
 		internal_table->setModel(const_cast<QAbstractItemModel*>(index.model()));
 		internal_table->setRootIndex(index);
