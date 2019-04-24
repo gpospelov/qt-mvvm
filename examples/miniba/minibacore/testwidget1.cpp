def1e8d12f800ce9b7c5a0352f4b6b7daca1ccdc
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-04-24 15:35:26 +0200

    Placeholders for mini BornAgain related playground

---
+++ examples/miniba/minibacore/testwidget1.cpp
@@ -0,0 +1,96 @@
+// ************************************************************************** //
+//
+//  Prototype of mini MVVM framework for bornagainproject.org
+//
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher
+//
+// ************************************************************************** //
+
+#include "testwidget1.h"
+#include <QStandardItem>
+#include <QStandardItemModel>
+#include <QTreeView>
+#include <QTableView>
+#include <QVBoxLayout>
+#include <QItemEditorFactory>
+#include <QStandardItemEditorCreator>
+#include <QColorDialog>
+#include <QDebug>
+
+namespace
+{
+QList<QStandardItem*> get_items(int n_items, const QString& prefix)
+{
+    QList<QStandardItem*> result;
+
+    for (int col = 0; col < n_items; ++col)
+        result.append(new QStandardItem(QString("%0 %1").arg(prefix).arg(col)));
+
+    return result;
+}
+
+QList<QStandardItem*> color_items(int n_items, const QString& prefix)
+{
+    QList<QStandardItem*> result;
+
+    for (int col = 0; col < n_items; ++col) {
+        auto item = new QStandardItem(QString("%0 %1").arg(prefix).arg(col));
+        item->setData(QColor(Qt::red), Qt::DecorationRole);
+        result.append(item);
+    }
+
+    return result;
+}
+
+}
+
+TestWidget1::TestWidget1(QWidget* parent)
+    : QWidget(parent), m_treeView(new QTreeView), m_tableView(new QTableView), m_model(new QStandardItemModel(this))
+{
+    auto layout = new QVBoxLayout;
+
+//    m_model->setColumnCount(3);
+    QStandardItem* parentItem = m_model->invisibleRootItem();
+
+//    auto item_row1 = get_items(1, "aaa");
+//    parentItem->appendRow(item_row1);
+//    item_row1.at(0)->appendRow(get_items(2, "bbb"));
+
+//    auto item_row2 = get_items(3, "aaa");
+//    parentItem->appendRow(item_row2);
+//    item_row2.at(0)->appendRow(get_items(2, "ccc"));
+
+
+
+    QItemEditorFactory *factory = new QItemEditorFactory;
+
+    QItemEditorCreatorBase *colorListCreator =
+        new QStandardItemEditorCreator<QColorDialog>();
+
+    factory->registerEditor(QVariant::Color, colorListCreator);
+
+//    QItemEditorFactory::setDefaultFactory(factory);
+
+
+    parentItem->appendRow(get_items(5, "aaa"));
+    parentItem->appendRow(get_items(5, "bbb"));
+    parentItem->appendRow(color_items(5, "ccc"));
+
+    m_treeView->setModel(m_model);
+    m_tableView->setModel(m_model);
+
+    m_tableView->setSpan(0, 0, 2, 1);
+
+    layout->addWidget(m_treeView);
+    layout->addWidget(m_tableView);
+
+    setLayout(layout);
+
+    connect(m_model, &QStandardItemModel::dataChanged, this, &TestWidget1::onDataChanged);
+}
+
+void TestWidget1::onDataChanged(const QModelIndex& topLeft, const QModelIndex& bottomRight, const QVector<int>& roles)
+{
+    qDebug() << topLeft << bottomRight << roles;
+}
---
+++ examples/miniba/minibacore/testwidget1.h
@@ -0,0 +1,36 @@
+// ************************************************************************** //
+//
+//  Prototype of mini MVVM framework for bornagainproject.org
+//
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher
+//
+// ************************************************************************** //
+
+#ifndef TESTWIDGET1_H
+#define TESTWIDGET1_H
+
+#include <QWidget>
+
+class QTreeView;
+class QTableView;
+class QStandardItemModel;
+
+//! Playground to test basics of QStandardItemModel.
+
+class TestWidget1 : public QWidget
+{
+    Q_OBJECT
+public:
+    TestWidget1(QWidget* parent = nullptr);
+
+public slots:
+    void onDataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight, const QVector<int> &roles);
+
+private:
+    QTreeView* m_treeView;
+    QTableView* m_tableView;
+    QStandardItemModel* m_model;
+};
+
+#endif // TESTWIDGET1_H
