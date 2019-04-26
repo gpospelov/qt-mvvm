6ad0a315bc974a4fa0bd98c56bf29c67d9f7816c
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-04-26 17:27:04 +0200

    Implement unit tests for ViewDataItem based on color

--- tests/viewmodel/TestViewDataItem.cpp
+++ tests/viewmodel/TestViewDataItem.cpp
@@ -23,10 +23,10 @@ TEST_F(TestViewDataItem, initialState)
     EXPECT_EQ(viewItem.item_role(), ItemDataRole::DATA);
 }
 
-//! ViewDataItem::data method
+//! ViewDataItem::data method for double values.
 //! Checks that the data method is correctly forwarded to underlying SessionItem.
 
-TEST_F(TestViewDataItem, data)
+TEST_F(TestViewDataItem, dataForDouble)
 {
     // create SessionItem with data on board
     std::unique_ptr<SessionItem> item = std::make_unique<SessionItem>();
@@ -39,10 +39,10 @@ TEST_F(TestViewDataItem, data)
     EXPECT_EQ(viewItem.data(Qt::DisplayRole), expected);
 }
 
-//! ViewDataItem::setData
+//! ViewDataItem::setData for double values.
 //! Checks that the setData method is correctly forwarded to underlying SessionItem.
 
-TEST_F(TestViewDataItem, setData)
+TEST_F(TestViewDataItem, setDataForDouble)
 {
     // create SessionItem with data on board
     std::unique_ptr<SessionItem> item = std::make_unique<SessionItem>();
@@ -63,3 +63,46 @@ TEST_F(TestViewDataItem, setData)
     QVariant not_allowed_value("Layer");
     EXPECT_THROW(viewItem.setData(not_allowed_value, Qt::EditRole), std::runtime_error);
 }
+
+//! ViewDataItem::data method for QColor.
+//! Checks that the data method is correctly forwarded to underlying SessionItem.
+
+TEST_F(TestViewDataItem, dataForColor)
+{
+    // create SessionItem with data on board
+    std::unique_ptr<SessionItem> item = std::make_unique<SessionItem>();
+    QVariant expected = QVariant::fromValue(QColor(Qt::green));
+    EXPECT_TRUE(item->setData(expected, ItemDataRole::DATA));
+
+    ViewDataItem viewItem(item.get());
+    EXPECT_EQ(viewItem.data(Qt::EditRole), expected);
+    EXPECT_EQ(viewItem.data(Qt::DisplayRole), expected);
+    EXPECT_EQ(viewItem.data(Qt::DecorationRole), expected);
+}
+
+//! ViewDataItem::setData for QColor.
+//! Checks that the setData method is correctly forwarded to underlying SessionItem.
+
+TEST_F(TestViewDataItem, setDataForColor)
+{
+    // create SessionItem with data on board
+    std::unique_ptr<SessionItem> item = std::make_unique<SessionItem>();
+    QVariant expected = QVariant::fromValue(QColor(Qt::green));
+    EXPECT_TRUE(item->setData(expected, ItemDataRole::DATA));
+
+    // initialize viewItem with sessionItem and set the data
+    ViewDataItem viewItem(item.get());
+    QVariant new_data = QVariant::fromValue(QColor(Qt::red));
+    viewItem.setData(new_data, Qt::EditRole);
+    EXPECT_EQ(viewItem.data(Qt::DisplayRole), new_data); // new data
+    EXPECT_EQ(viewItem.data(Qt::EditRole), new_data);    // new data
+    EXPECT_EQ(viewItem.data(Qt::DecorationRole), new_data);    // new data
+
+    // SessionItem itself should have new data
+    EXPECT_EQ(item->data(ItemDataRole::DATA), new_data); // new data
+
+    // it is not allowed to set another type of data to ViewDataItem
+    QVariant not_allowed_value("Layer");
+    EXPECT_THROW(viewItem.setData(not_allowed_value, Qt::EditRole), std::runtime_error);
+}
+
