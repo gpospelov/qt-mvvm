04c3a1b8f40716cfc66c608edfd22fc1eb34be95
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-04-26 17:17:11 +0200

    Extract ViewDataItem unit tests to separate file

---
+++ tests/viewmodel/TestViewDataItem.cpp
@@ -0,0 +1,65 @@
+#include "google_test.h"
+#include "sessionitem.h"
+#include "viewdataitem.h"
+#include <QDebug>
+#include <memory>
+
+using namespace ModelView;
+
+class TestViewDataItem : public ::testing::Test
+{
+public:
+    ~TestViewDataItem();
+};
+
+TestViewDataItem::~TestViewDataItem() = default;
+
+TEST_F(TestViewDataItem, initialState)
+{
+    std::unique_ptr<SessionItem> item(new SessionItem);
+
+    ViewDataItem viewItem(item.get());
+    EXPECT_EQ(viewItem.item(), item.get());
+    EXPECT_EQ(viewItem.item_role(), ItemDataRole::DATA);
+}
+
+//! ViewDataItem::data method
+//! Checks that the data method is correctly forwarded to underlying SessionItem.
+
+TEST_F(TestViewDataItem, data)
+{
+    // create SessionItem with data on board
+    std::unique_ptr<SessionItem> item = std::make_unique<SessionItem>();
+    QVariant expected(42.0);
+    EXPECT_TRUE(item->setData(expected, ItemDataRole::DATA));
+
+    // initialize viewItem with sessionItem and check the data
+    ViewDataItem viewItem(item.get());
+    EXPECT_EQ(viewItem.data(Qt::EditRole), expected);
+    EXPECT_EQ(viewItem.data(Qt::DisplayRole), expected);
+}
+
+//! ViewDataItem::setData
+//! Checks that the setData method is correctly forwarded to underlying SessionItem.
+
+TEST_F(TestViewDataItem, setData)
+{
+    // create SessionItem with data on board
+    std::unique_ptr<SessionItem> item = std::make_unique<SessionItem>();
+    QVariant expected(42.0);
+    EXPECT_TRUE(item->setData(expected, ItemDataRole::DATA));
+
+    // initialize viewItem with sessionItem and set the data
+    ViewDataItem viewItem(item.get());
+    QVariant new_data(43.0);
+    viewItem.setData(new_data, Qt::EditRole);
+    EXPECT_EQ(viewItem.data(Qt::DisplayRole), new_data); // new data
+    EXPECT_EQ(viewItem.data(Qt::EditRole), new_data);    // new data
+
+    // SessionItem itself should have new data
+    EXPECT_EQ(item->data(ItemDataRole::DATA), new_data); // new data
+
+    // it is not allowed to set another type of data to ViewDataItem
+    QVariant not_allowed_value("Layer");
+    EXPECT_THROW(viewItem.setData(not_allowed_value, Qt::EditRole), std::runtime_error);
+}
--- tests/viewmodel/TestViewItems.cpp
+++ tests/viewmodel/TestViewItems.cpp
@@ -1,7 +1,6 @@
 #include "google_test.h"
 #include "sessionitem.h"
 #include "viewitems.h"
-#include "viewdataitem.h"
 #include <QDebug>
 #include <memory>
 
@@ -15,15 +14,6 @@ public:
 
 TestViewItem::~TestViewItem() = default;
 
-TEST_F(TestViewItem, initialViewDataItem)
-{
-    std::unique_ptr<SessionItem> item(new SessionItem);
-
-    ViewDataItem viewItem(item.get());
-    EXPECT_EQ(viewItem.item(), item.get());
-    EXPECT_EQ(viewItem.item_role(), ItemDataRole::DATA);
-}
-
 TEST_F(TestViewItem, initialViewLabelItem)
 {
     std::unique_ptr<SessionItem> item(new SessionItem);
@@ -31,22 +21,6 @@ TEST_F(TestViewItem, initialViewLabelItem)
     ViewLabelItem viewItem(item.get());
     EXPECT_EQ(viewItem.item(), item.get());
     EXPECT_EQ(viewItem.item_role(), ItemDataRole::DISPLAY);
-}
-
-//! ViewDataItem::data method
-//! Checks that the data method is correctly forwarded to underlying SessionItem.
-
-TEST_F(TestViewItem, ViewDataItem_data)
-{
-    // create SessionItem with data on board
-    std::unique_ptr<SessionItem> item = std::make_unique<SessionItem>();
-    QVariant expected(42.0);
-    EXPECT_TRUE(item->setData(expected, ItemDataRole::DATA));
-
-    // initialize viewItem with sessionItem and check the data
-    ViewDataItem viewItem(item.get());
-    EXPECT_EQ(viewItem.data(Qt::EditRole), expected);
-    EXPECT_EQ(viewItem.data(Qt::DisplayRole), expected);
 }
 
 //! ViewLabelItem::data method
@@ -63,31 +37,6 @@ TEST_F(TestViewItem, ViewLabelItem_data)
     ViewLabelItem viewItem(item.get());
     EXPECT_EQ(Utils::toCustomVariant(viewItem.data(Qt::EditRole)), expected);
     EXPECT_EQ(Utils::toCustomVariant(viewItem.data(Qt::DisplayRole)), expected);
-}
-
-//! ViewDataItem::setData
-//! Checks that the setData method is correctly forwarded to underlying SessionItem.
-
-TEST_F(TestViewItem, ViewDataItem_setData)
-{
-    // create SessionItem with data on board
-    std::unique_ptr<SessionItem> item = std::make_unique<SessionItem>();
-    QVariant expected(42.0);
-    EXPECT_TRUE(item->setData(expected, ItemDataRole::DATA));
-
-    // initialize viewItem with sessionItem and set the data
-    ViewDataItem viewItem(item.get());
-    QVariant new_data(43.0);
-    viewItem.setData(new_data, Qt::EditRole);
-    EXPECT_EQ(viewItem.data(Qt::DisplayRole), new_data); // new data
-    EXPECT_EQ(viewItem.data(Qt::EditRole), new_data);    // new data
-
-    // SessionItem itself should have new data
-    EXPECT_EQ(item->data(ItemDataRole::DATA), new_data); // new data
-
-    // it is not allowed to set another type of data to ViewDataItem
-    QVariant not_allowed_value("Layer");
-    EXPECT_THROW(viewItem.setData(not_allowed_value, Qt::EditRole), std::runtime_error);
 }
 
 //! ViewLabelItem::setData
