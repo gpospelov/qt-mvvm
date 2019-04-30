a7e02ac4d902ba2450474bd455001cf381dbf434
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-04-30 15:45:06 +0200

    Connect MaterialSelectorCellEditor with MaterialModel.

--- examples/miniba/minibacore/viewmodel/MaterialSelectorCellEditor.cpp
+++ examples/miniba/minibacore/viewmodel/MaterialSelectorCellEditor.cpp
@@ -8,26 +8,42 @@
 // ************************************************************************** //
 
 #include "MaterialSelectorCellEditor.h"
+#include "MaterialItems.h"
+#include "MaterialModel.h"
 #include "externalproperty.h"
+#include <QColor>
 #include <QComboBox>
-#include <QVBoxLayout>
 #include <QDebug>
+#include <QStandardItemModel>
+#include <QVBoxLayout>
 
 namespace
 {
-QStringList toList(const std::vector<std::string>& container)
+std::vector<ModelView::ExternalProperty> material_data(ModelView::SessionModel* model)
 {
-    QStringList result;
-    for (auto str : container)
-        result.push_back(QString::fromStdString(str));
+    std::vector<ModelView::ExternalProperty> result;
+
+    result.push_back(ModelView::ExternalProperty("Undefined", QColor(Qt::red)));
+    for (auto container : model->rootItem()->children()) {
+        for (auto item : container->children()) {
+            if (auto material = dynamic_cast<SLDMaterialItem*>(item)) {
+                auto text = material->getItemValue(SLDMaterialItem::P_NAME).value<std::string>();
+                auto color = material->getItemValue(SLDMaterialItem::P_COLOR).value<QColor>();
+                auto id = material->identifier();
+                result.push_back(ModelView::ExternalProperty(text, color, id));
+            }
+        }
+    }
     return result;
 }
+
 } // namespace
 
 using namespace ModelView;
 
 MaterialSelectorCellEditor::MaterialSelectorCellEditor(SessionModel* model, QWidget* parent)
-    : CustomEditor(parent), m_box(new QComboBox), m_model(model)
+    : CustomEditor(parent), m_box(new QComboBox), m_model(model),
+      m_combo_model(new QStandardItemModel(this))
 {
     setAutoFillBackground(true);
     setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
@@ -37,6 +53,8 @@ MaterialSelectorCellEditor::MaterialSelectorCellEditor(SessionModel* model, QWid
     layout->setSpacing(0);
     layout->addWidget(m_box);
     setLayout(layout);
+
+    m_box->setModel(m_combo_model);
 
     setConnected(true);
 }
@@ -53,32 +71,51 @@ QSize MaterialSelectorCellEditor::minimumSizeHint() const
 
 void MaterialSelectorCellEditor::onIndexChanged(int index)
 {
-    qDebug() << "MaterialSelectorCellEditor::onIndexChanged" << index;
+    qDebug() << "MaterialSelectorCellEditor::onIndexChanged(int index)" << index;
+
+    auto property = m_data.value<ModelView::ExternalProperty>();
+    auto mdata = material_data(m_model);
+
+    if (index >= 0 && index < static_cast<int>(mdata.size())) {
+        if (property != mdata[static_cast<size_t>(index)])
+            setDataIntern(QVariant::fromValue(mdata[index]));
+    }
 }
 
 void MaterialSelectorCellEditor::update_components()
 {
     setConnected(false);
 
-    m_box->clear();
-    m_box->insertItems(0, toList(internLabels()));
+    m_combo_model->clear();
+
+    QStandardItem* parentItem = m_combo_model->invisibleRootItem();
+    for (auto prop : material_data(m_model)) {
+        auto item = new QStandardItem(QString::fromStdString(prop.text()));
+        parentItem->appendRow(item);
+        item->setData(prop.color(), Qt::DecorationRole);
+    }
+
     m_box->setCurrentIndex(internIndex());
 
     setConnected(true);
-}
-
-//! Returns list of labels for QComboBox
-
-std::vector<std::string> MaterialSelectorCellEditor::internLabels()
-{
-    return {"aaa"};
 }
 
 //! Returns index for QComboBox.
 
 int MaterialSelectorCellEditor::internIndex()
 {
-    return 0;
+    if (!m_data.canConvert<ModelView::ExternalProperty>())
+        return 0;
+
+    auto property = m_data.value<ModelView::ExternalProperty>();
+    int result(-1);
+    for (auto prop : material_data(m_model)) {
+        ++result;
+        if (property.identifier() == prop.identifier())
+            return result;
+    }
+
+    return result;
 }
 
 void MaterialSelectorCellEditor::setConnected(bool isConnected)
--- examples/miniba/minibacore/viewmodel/MaterialSelectorCellEditor.h
+++ examples/miniba/minibacore/viewmodel/MaterialSelectorCellEditor.h
@@ -13,11 +13,14 @@
 #include "customeditor.h"
 
 class QComboBox;
+class QStandardItemModel;
+class MaterialModel;
 
 namespace ModelView
 {
 class SessionModel;
-}
+class ExternalProperty;
+} // namespace ModelView
 
 //! Custom editor for table cells to select material from the list of existing materials.
 
@@ -33,12 +36,12 @@ protected slots:
     virtual void onIndexChanged(int index);
 
 private:
-    std::vector<std::string> internLabels();
     int internIndex();
     void setConnected(bool isConnected);
     void update_components() override;
     QComboBox* m_box;
     ModelView::SessionModel* m_model;
+    QStandardItemModel* m_combo_model;
 };
 
 #endif // MATERIALSELECTORCELLEDITOR_H
