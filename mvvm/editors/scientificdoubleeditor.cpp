87d996ade4991036ebd4dcab85acef831713dfa0
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-05-15 13:26:23 +0200

    Migrate scientific double editor

---
+++ mvvm/editors/scientificdoubleeditor.cpp
@@ -0,0 +1,59 @@
+// ************************************************************************** //
+//
+//  Prototype of mini MVVM framework for bornagainproject.org
+//
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher
+//
+// ************************************************************************** //
+
+#include "scientificdoubleeditor.h"
+#include <QDoubleValidator>
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <cmath>
+#include <limits>
+#include <stdexcept>
+
+using namespace ModelView;
+
+ScientificDoubleEditor::ScientificDoubleEditor(QWidget* parent)
+    : CustomEditor(parent), m_lineEdit(new QLineEdit), m_validator(nullptr)
+
+{
+    setAutoFillBackground(true);
+
+    auto layout = new QVBoxLayout;
+    layout->setMargin(0);
+    layout->setSpacing(0);
+
+    layout->addWidget(m_lineEdit);
+
+    m_validator = new QDoubleValidator(0.0, 1e+200, 1000, this);
+    m_validator->setNotation(QDoubleValidator::ScientificNotation);
+    m_lineEdit->setValidator(m_validator);
+
+    connect(m_lineEdit, &QLineEdit::editingFinished, this,
+            &ScientificDoubleEditor::onEditingFinished);
+
+    setLayout(layout);
+}
+
+void ScientificDoubleEditor::setLimits(const RealLimits&) {}
+
+void ScientificDoubleEditor::onEditingFinished()
+{
+    double new_value = m_lineEdit->text().toDouble();
+
+    if (std::fabs(new_value - m_data.toDouble()) < std::numeric_limits<double>::epsilon())
+        setDataIntern(QVariant::fromValue(new_value));
+}
+
+void ScientificDoubleEditor::update_components()
+{
+    if (m_data.type() != QVariant::Double)
+        throw std::runtime_error(
+            "ScientificDoubleEditor::update_components() -> Error. Wrong variant type");
+
+    m_lineEdit->setText(QString::number(m_data.toDouble(), 'g'));
+}
---
+++ mvvm/editors/scientificdoubleeditor.h
@@ -0,0 +1,43 @@
+// ************************************************************************** //
+//
+//  Prototype of mini MVVM framework for bornagainproject.org
+//
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher
+//
+// ************************************************************************** //
+
+#ifndef MVVM_SCIENTIFICDOUBLEEDITOR_H
+#define MVVM_SCIENTIFICDOUBLEEDITOR_H
+
+#include "customeditor.h"
+
+class QLineEdit;
+class QDoubleValidator;
+
+namespace ModelView
+{
+
+class RealLimits;
+
+//! Custom editor for QVariant based on double with scientific notation support.
+
+class CORE_EXPORT ScientificDoubleEditor : public CustomEditor
+{
+public:
+    explicit ScientificDoubleEditor(QWidget* parent = nullptr);
+
+    void setLimits(const RealLimits& limits);
+
+private slots:
+    void onEditingFinished();
+
+private:
+    void update_components() override;
+    QLineEdit* m_lineEdit;
+    QDoubleValidator* m_validator;
+};
+
+} // namespace ModelView
+
+#endif // MVVM_SCIENTIFICDOUBLEEDITOR_H
