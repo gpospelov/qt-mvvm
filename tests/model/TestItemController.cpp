22dcd08af046ee50048fbedc7439ab85ea57742c
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-09-25 15:19:28 +0200

    Provide unit tests for second version of controller

--- tests/model/TestItemController.cpp
+++ tests/model/TestItemController.cpp
@@ -3,6 +3,7 @@
 #include "itemmapper.h"
 #include "sessionitem.h"
 #include "sessionmodel.h"
+#include "propertyitem.h"
 #include <memory>
 
 using namespace ModelView;
@@ -22,6 +23,18 @@ public:
             currentItem()->mapper()->setOnDataChange(on_data_change, this);
         }
     };
+
+    class TestControllerV2 : public ItemControllerV2<PropertyItem>
+    {
+    public:
+        int ncount{0};
+        void subscribe()
+        {
+            auto on_data_change = [this](SessionItem*, int) { ncount++; };
+            currentItem()->mapper()->setOnDataChange(on_data_change, this);
+        }
+    };
+
     ~TestItemController();
 };
 
@@ -57,6 +70,45 @@ TEST_F(TestItemController, controllerBeforeItem)
     auto item = model.insertNewItem(Constants::BaseType);
 
     std::unique_ptr<TestController> controller = std::make_unique<TestController>();
+    controller->setItem(item);
+    EXPECT_EQ(controller->currentItem(), item);
+
+    controller.reset();
+    item->setData(42.0);
+}
+
+// ---
+
+//! Initial state.
+
+TEST_F(TestItemController, initialStateV2)
+{
+    TestControllerV2 controller;
+    EXPECT_EQ(controller.currentItem(), nullptr);
+}
+
+TEST_F(TestItemController, itemBeforeControllerV2)
+{
+    SessionModel model;
+    auto item = dynamic_cast<PropertyItem*>(model.insertNewItem(Constants::PropertyType));
+
+    std::unique_ptr<TestControllerV2> controller = std::make_unique<TestControllerV2>();
+    controller->setItem(item);
+    item->setData(42.0);
+
+    EXPECT_EQ(controller->currentItem(), item);
+    EXPECT_EQ(controller->ncount, 1);
+
+    model.removeItem(model.rootItem(), "", 0);
+    EXPECT_EQ(controller->currentItem(), nullptr);
+}
+
+TEST_F(TestItemController, controllerBeforeItemV2)
+{
+    SessionModel model;
+    auto item = dynamic_cast<PropertyItem*>(model.insertNewItem(Constants::PropertyType));
+
+    std::unique_ptr<TestControllerV2> controller = std::make_unique<TestControllerV2>();
     controller->setItem(item);
     EXPECT_EQ(controller->currentItem(), item);
 
