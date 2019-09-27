ee4754fece8edaca77dc72b40cc13f7000206d6b
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-09-27 12:03:39 +0200

    Creates graph controllers on view port setup.

--- mvvm/plotting/graphcollectionplotcontroller.cpp
+++ mvvm/plotting/graphcollectionplotcontroller.cpp
@@ -9,18 +9,30 @@
 
 #include "graphcollectionplotcontroller.h"
 #include "graphviewportitem.h"
+#include "graphplotcontroller.h"
 
 using namespace ModelView;
 
 struct GraphCollectionPlotController::GraphCollectionPlotControllerPrivate {
     GraphCollectionPlotController* master{nullptr};
     QCustomPlot* custom_plot{nullptr};
+    std::vector<std::unique_ptr<GraphPlotController>> graph_controllers;
 
     GraphCollectionPlotControllerPrivate(GraphCollectionPlotController* master, QCustomPlot* plot)
         : master(master), custom_plot(plot)
     {
     }
 
+    GraphViewportItem* viewport_item() { return master->currentItem(); }
+
+    //! Run through all GraphItem and populate
+    void setup_graphs() {
+        auto viewport = viewport_item();
+        for (auto graph_item : viewport->items<GraphItem>(GraphViewportItem::T_GRAPHS)) {
+            auto controller = std::make_unique<GraphPlotController>(custom_plot);
+            controller->setItem(graph_item);
+        }
+    }
 };
 
 GraphCollectionPlotController::GraphCollectionPlotController(QCustomPlot* custom_plot)
@@ -30,6 +42,7 @@ GraphCollectionPlotController::GraphCollectionPlotController(QCustomPlot* custom
 
 void GraphCollectionPlotController::subscribe()
 {
+    p_impl->setup_graphs();
 }
 
 GraphCollectionPlotController::~GraphCollectionPlotController() = default;
