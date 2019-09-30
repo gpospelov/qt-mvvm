c734b1208b52976f6b372318ac13c99d4271a67b
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-09-30 18:35:35 +0200

    Avoid automatic display name generation if custom display name has been already set.

--- mvvm/model/compounditem.cpp
+++ mvvm/model/compounditem.cpp
@@ -15,10 +15,21 @@
 
 using namespace ModelView;
 
+namespace
+{
+bool has_custom_display_name(const SessionItem* item)
+{
+    return item->SessionItem::displayName() != item->modelType();
+}
+} // namespace
+
 CompoundItem::CompoundItem(const std::string& modelType) : SessionItem(modelType) {}
 
 std::string CompoundItem::displayName() const
 {
+    if (has_custom_display_name(this))
+        return SessionItem::displayName();
+
     int copy_number = Utils::CopyNumber(this);
     return copy_number != -1 ? SessionItem::displayName() + std::to_string(copy_number)
                              : SessionItem::displayName();
