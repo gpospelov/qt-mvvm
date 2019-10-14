1bf693371a94989073298905865e7999b8fc6a6c
Dmitry Yurov <d.yurov@fz-juelich.de>
2019-10-14 15:17:43 +0200

    Implement GraphicsObjectController

--- examples/sampleview/core/sampledesigner/GraphicsObjectController.cpp
+++ examples/sampleview/core/sampledesigner/GraphicsObjectController.cpp
@@ -8,8 +8,175 @@
 // ************************************************************************** //
 
 #include "GraphicsObjectController.h"
+#include "ConnectableView.h"
+#include "DesignerScene.h"
+#include "NodeEditorConnection.h"
+#include "SampleModel.h"
+#include "modelmapper.h"
+#include "modelutils.h"
+#include "sessionitem.h"
 
-GraphicsObjectController::GraphicsObjectController(QObject *parent) : QObject(parent)
+using namespace ModelView;
+
+namespace {
+class BlockMutex
 {
+public:
+    BlockMutex(bool& block) : m_block(block)
+    {
+        if (block)
+            throw std::runtime_error("Error in BlockMutex: already blocked.");
+        m_block = true;
+    }
 
+    ~BlockMutex() { m_block = false; }
+
+private:
+    bool& m_block;
+};
+
+class ModelCommandExecutor
+{
+public:
+    ModelCommandExecutor(GraphicsObjectController::ModelCommand& command, SessionModel* model)
+        : m_command(command)
+        , m_model(model)
+    {}
+
+    ~ModelCommandExecutor() { m_command = GraphicsObjectController::ModelCommand(); }
+
+    void execute() {
+        if (!m_model || !m_command)
+            return;
+        m_command(*m_model);
+    }
+
+private:
+    DesignerScene::ModelCommand& m_command;
+    SessionModel* m_model;
+};
+}
+
+GraphicsObjectController::GraphicsObjectController(DesignerScene& scene, SampleModel* model)
+    : m_scene(scene)
+    , m_model(model)
+    , m_block(false)
+{
+    if (!m_model)
+        throw std::runtime_error("Error in GraphicsObjectController: passed model is null.");
+
+    m_model->mapper()->setOnModelReset([this](SessionModel*) { onModelChange(); }, this);
+    m_model->mapper()->setOnModelDestroyed([this](SessionModel*) { onModelDestroyed(); }, this);
+    m_model->mapper()->setOnRowInserted([this](SessionItem*, std::string, int) { onModelChange(); },
+                                        this);
+    m_model->mapper()->setOnRowRemoved([this](SessionItem*, std::string, int) { onModelChange(); },
+                                       this);
+}
+
+GraphicsObjectController::~GraphicsObjectController()
+{
+    m_model->mapper()->unsubscribe(this);
+}
+
+void GraphicsObjectController::onDelete(QList<QGraphicsItem *> views)
+{
+    if (!m_model)
+        return;
+    BlockMutex signal_guard(m_block);
+
+    //! Disconnect dependent items
+    const auto connected_views = findConnectedViews(views);
+    for (auto view: connected_views)
+        m_model->moveItem(view->getItem(), m_model->rootItem(), {}, -1);
+
+    const auto top_views = findTopViews(views);
+    for (auto view: top_views)
+        Utils::DeleteItemFromModel(view->getItem());
+
+    m_scene.onModelChanged();
+}
+
+void GraphicsObjectController::onConnect(NodeEditorConnection *connection)
+{
+    if (!m_model)
+        return;
+    BlockMutex signal_guard(m_block);
+
+    ConnectableView* parentView = connection->getParentView();
+    ConnectableView* childView = connection->getChildView();
+
+    m_model->moveItem(childView->getItem(), parentView->getItem(), {}, -1);
+    m_scene.onModelChanged();
+}
+
+void GraphicsObjectController::setDelayedCommand(GraphicsObjectController::ModelCommand command)
+{
+    if (m_command)
+        throw std::runtime_error(
+            "Error in GraphicsObjectController::setDelayedCommand: command was already set.");
+
+    m_command = command;
+}
+
+void GraphicsObjectController::executeDelayedCommand()
+{
+    ModelCommandExecutor executor(m_command, m_model);
+    executor.execute();
+    m_scene.onModelChanged();
+}
+
+void GraphicsObjectController::onModelChange()
+{
+    if (!m_block)
+        m_scene.onModelChanged();
+}
+
+void GraphicsObjectController::onModelDestroyed()
+{
+    m_model = nullptr;
+    m_scene.onModelDestroyed();
+}
+
+std::set<IView*>
+GraphicsObjectController::findConnectedViews(QList<QGraphicsItem*> views) const
+{
+    std::set<IView*> connected_views;
+
+    // first add child views from the input ports of the selected connections
+    for (auto view : views)
+        if (auto connection = dynamic_cast<NodeEditorConnection*>(view))
+            connected_views.insert(connection->getChildView());
+
+    // now add the views of child session items
+    for (auto view : views) {
+        auto iview = dynamic_cast<IView*>(view);
+        if (!iview || !iview->getItem())
+            continue;
+        SessionItem* item = iview->getItem();
+        for (auto child : item->children()) {
+            auto child_view = m_scene.getViewForItem(child);
+            if (child_view && child_view->parentObject() != iview)
+                connected_views.insert(child_view);
+        }
+    }
+
+    return connected_views;
+}
+
+std::set<IView*> GraphicsObjectController::findTopViews(QList<QGraphicsItem*> views) const
+{
+    std::set<IView *> result;
+    for (QGraphicsItem* view: views) {
+        auto iview = dynamic_cast<IView*>(view);
+        if (!iview || !iview->getItem())
+            continue;
+        const bool has_ancestors =
+            std::accumulate(views.begin(), views.end(), false,
+                            [view](bool result, QGraphicsItem* pview) {
+                                return result || pview->isAncestorOf(view);
+                            });
+        if (!has_ancestors)
+            result.insert(iview);
+    }
+    return result;
 }
--- examples/sampleview/core/sampledesigner/GraphicsObjectController.h
+++ examples/sampleview/core/sampledesigner/GraphicsObjectController.h
@@ -10,13 +10,52 @@
 #ifndef GRAPHICSOBJECTCONTROLLER_H
 #define GRAPHICSOBJECTCONTROLLER_H
 
-#include <QObject>
+#include <QList>
+#include <set>
 
-class GraphicsObjectController : public QObject
+namespace ModelView {
+class SessionModel;
+}
+
+class DesignerScene;
+class IView;
+class NodeEditorConnection;
+class QGraphicsItem;
+class SampleModel;
+
+//! Manages scene-model communication.
+class GraphicsObjectController
 {
-    Q_OBJECT
 public:
-    explicit GraphicsObjectController(QObject *parent = nullptr);
+    using ModelCommand = std::function<void (ModelView::SessionModel& model)>;
+
+    explicit GraphicsObjectController(DesignerScene& scene, SampleModel* model);
+    ~GraphicsObjectController();
+
+    //! Propagates deletion of views on the scene to the model
+    void onDelete(QList<QGraphicsItem*> views);
+    //! Propagates view connection to the model
+    void onConnect(NodeEditorConnection* connection);
+
+    const SampleModel* model() const { return m_model; }
+
+    //! Sets a command to model for later execution
+    void setDelayedCommand(ModelCommand command);
+    void executeDelayedCommand();
+
+private:
+    void onModelChange();
+    void onModelDestroyed();
+
+    //! Finds all the views connected to the passed list
+    std::set<IView*> findConnectedViews(QList<QGraphicsItem*> views) const;
+    //! Finds all the views with no ancestor in the passed list
+    std::set<IView*> findTopViews(QList<QGraphicsItem*> views) const;
+
+    DesignerScene& m_scene;
+    SampleModel* m_model;
+    bool m_block;
+    ModelCommand m_command;
 };
 
 #endif // GRAPHICSOBJECTCONTROLLER_H
