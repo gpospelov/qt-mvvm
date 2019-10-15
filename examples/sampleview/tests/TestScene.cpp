8da26d5446336875f956c3ddbe312b9e06046a90
Dmitry Yurov <d.yurov@fz-juelich.de>
2019-10-15 13:13:26 +0200

    Expand scene unit tests

--- examples/sampleview/tests/TestScene.cpp
+++ examples/sampleview/tests/TestScene.cpp
@@ -19,6 +19,7 @@ public:
 
 protected:
     QRectF makeFrame(QRectF bound);
+    QRectF selectSpaceBetween(const QRectF& left, const QRectF& right);
 
     SampleModel model;
     std::unique_ptr<DesignerScene> scene;
@@ -35,6 +36,21 @@ TestScene::~TestScene() = default;
 QRectF TestScene::makeFrame(QRectF bound)
 {
     return QRectF(bound.left() - 0.1, bound.top() - 0.1, bound.width() + 0.2, bound.height() + 0.2);
+}
+
+QRectF TestScene::selectSpaceBetween(const QRectF& left, const QRectF& right)
+{
+    const qreal x_left = left.right() + 0.1;
+    const qreal y_top = std::max(left.top(), right.top());
+    const qreal width = right.left() - left.right() - 0.2;
+    const qreal height = std::max(left.height(), right.height());
+    return QRectF(x_left, y_top, width, height);
+}
+
+TEST_F(TestScene, testEmptyScene)
+{
+    auto views = scene->views();
+    EXPECT_TRUE(views.empty());
 }
 
 TEST_F(TestScene, testSelection)
@@ -66,7 +82,7 @@ TEST_F(TestScene, testSelection)
     EXPECT_TRUE(scene->selectedItems().contains(lview));
 }
 
-TEST_F(TestScene, testAddingConnection)
+TEST_F(TestScene, testAddConnection)
 {
     const auto mlayer = model.insertItem<MultiLayerItem>();
     const auto layer = model.insertItem<LayerItem>(mlayer, MultiLayerItem::T_LAYERS);
@@ -75,7 +91,6 @@ TEST_F(TestScene, testAddingConnection)
     EXPECT_TRUE(layout->parent() == model.rootItem());
 
     auto connection = new NodeEditorConnection(scene.get());
-    scene->addItem(connection);
     connection->setPort1(
         dynamic_cast<ConnectableView*>(scene->getViewForItem(layout))->getOutputPorts()[0]);
     connection->setPort2(
@@ -87,8 +102,7 @@ TEST_F(TestScene, testAddingConnection)
     EXPECT_TRUE(scene->getViewForItem(layout)->parentObject() != scene->getViewForItem(layer));
 }
 
-//! Tests deletion of views with model's items deletion
-TEST_F(TestScene, testBasicViewDeletion)
+TEST_F(TestScene, testDeleteLayer)
 {
     const auto mlayer = model.insertItem<MultiLayerItem>();
     const std::string mid = mlayer->identifier();
@@ -102,7 +116,90 @@ TEST_F(TestScene, testBasicViewDeletion)
     scene->deleteSelectedItems();
 
     EXPECT_TRUE(model.findItem(mid) == mlayer);
-    EXPECT_TRUE(model.findItem(lid) == nullptr);
-    EXPECT_TRUE(scene->getViewForItem(mlayer) != nullptr);
-    EXPECT_TRUE(scene->getViewForItem(layer) == nullptr);
+    EXPECT_FALSE(model.findItem(lid));
+    EXPECT_TRUE(scene->getViewForItem(mlayer));
+    EXPECT_FALSE(scene->getViewForItem(layer));
+}
+
+TEST_F(TestScene, testDeleteConnection)
+{
+    const auto mlayer = model.insertItem<MultiLayerItem>();
+    const std::string mid = mlayer->identifier();
+    const auto layer = model.insertItem<LayerItem>(mlayer, MultiLayerItem::T_LAYERS);
+    const std::string lid = layer->identifier();
+    const auto layout = model.insertItem<ParticleLayoutItem>(layer, LayerItem::T_LAYOUTS);
+    const std::string layout_id = layout->identifier();
+
+    auto layout_view = scene->getViewForItem(layout);
+    layout_view->moveBy(-300.0, 0.0); // move layout to make capturing the connection simpler
+
+    auto m_view = scene->getViewForItem(mlayer);
+
+    // selecting the connection between the layout and the layer
+    QPainterPath selection_path;
+    selection_path.addRect(
+        selectSpaceBetween(layout_view->sceneBoundingRect(), m_view->sceneBoundingRect()));
+    scene->setSelectionArea(selection_path, Qt::IntersectsItemShape);
+
+    auto selected = scene->selectedItems();
+    EXPECT_FALSE(selected.isEmpty());
+    NodeEditorConnection* connection =
+        selected.isEmpty() ? nullptr : dynamic_cast<NodeEditorConnection*>(selected.front());
+    EXPECT_TRUE(connection);
+
+    scene->deleteSelectedItems();
+
+    selected = scene->selectedItems();
+    EXPECT_TRUE(selected.isEmpty());
+
+    EXPECT_TRUE(model.findItem(mid) == mlayer);
+    EXPECT_TRUE(model.findItem(lid) == layer);
+    EXPECT_TRUE(model.findItem(layout_id) == layout);
+    EXPECT_TRUE(layout->parent() == model.rootItem());
+    EXPECT_TRUE(scene->getViewForItem(mlayer));
+    EXPECT_TRUE(scene->getViewForItem(layer));
+    EXPECT_TRUE(scene->getViewForItem(layout));
+}
+
+TEST_F(TestScene, testDeleteMultilayer)
+{
+    const auto mlayer = model.insertItem<MultiLayerItem>();
+    const std::string mid = mlayer->identifier();
+    const auto layer = model.insertItem<LayerItem>(mlayer, MultiLayerItem::T_LAYERS);
+    const std::string lid = layer->identifier();
+    const auto layout = model.insertItem<ParticleLayoutItem>(layer, LayerItem::T_LAYOUTS);
+    const std::string layout_id = layout->identifier();
+
+    auto layout_view = scene->getViewForItem(layout);
+    layout_view->moveBy(-300.0, 0.0); // move layout to make capturing the connection simpler
+
+    auto m_view = scene->getViewForItem(mlayer);
+
+    // selecting the multilayer (layer stays unselected)
+    QPainterPath selection_path;
+    QRectF selection_rect;
+    selection_rect.setX(m_view->sceneBoundingRect().right() - 5.0);
+    selection_rect.setY(m_view->sceneBoundingRect().top() - 5.0);
+    selection_rect.setSize({10.0, 10.0});
+    selection_path.addRect(selection_rect);
+    scene->setSelectionArea(selection_path, Qt::IntersectsItemBoundingRect);
+
+    auto selected = scene->selectedItems();
+    EXPECT_FALSE(selected.isEmpty());
+    MultiLayerView* multilayer_view =
+        selected.isEmpty() ? nullptr : dynamic_cast<MultiLayerView*>(selected.front());
+    EXPECT_TRUE(multilayer_view == m_view);
+
+    scene->deleteSelectedItems();
+
+    selected = scene->selectedItems();
+    EXPECT_TRUE(selected.isEmpty());
+
+    EXPECT_FALSE(model.findItem(mid));
+    EXPECT_FALSE(model.findItem(lid));
+    EXPECT_TRUE(model.findItem(layout_id) == layout);
+    EXPECT_TRUE(layout->parent() == model.rootItem());
+    EXPECT_FALSE(scene->getViewForItem(mlayer));
+    EXPECT_FALSE(scene->getViewForItem(layer));
+    EXPECT_TRUE(scene->getViewForItem(layout));
 }
