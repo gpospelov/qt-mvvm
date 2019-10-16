68a4c1cbc803164c1a0966aa8b22ef90dc3230d9
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-10-16 18:07:24 +0200

    Standard code beautification

--- mvvm/commands/copyitemcommand.cpp
+++ mvvm/commands/copyitemcommand.cpp
@@ -8,11 +8,11 @@
 // ************************************************************************** //
 
 #include "copyitemcommand.h"
+#include "itembackupstrategy.h"
+#include "itemcopystrategy.h"
 #include "path.h"
 #include "sessionitem.h"
 #include "sessionmodel.h"
-#include "itemcopystrategy.h"
-#include "itembackupstrategy.h"
 #include "tagrow.h"
 #include <sstream>
 
@@ -24,51 +24,46 @@ std::string generate_description(const std::string& modelType, const TagRow& tag
 } // namespace
 
 struct CopyItemCommand::CopyItemCommandPrivate {
-    TagRow m_tagrow;
-    result_t m_result;
-    std::unique_ptr<ItemBackupStrategy> m_backup_strategy;
-    Path m_item_path;
-    CopyItemCommandPrivate(TagRow tagrow)
-        : m_tagrow(std::move(tagrow)), m_result(nullptr)
-    {
-    }
+    TagRow tagrow;
+    result_t result;
+    std::unique_ptr<ItemBackupStrategy> backup_strategy;
+    Path item_path;
+    CopyItemCommandPrivate(TagRow tagrow) : tagrow(std::move(tagrow)), result(nullptr) {}
 };
 
-CopyItemCommand::CopyItemCommand(const SessionItem* item, SessionItem* parent,
-                                           TagRow tagrow)
-    : AbstractItemCommand(parent),
-      p_impl(std::make_unique<CopyItemCommandPrivate>(tagrow))
+CopyItemCommand::CopyItemCommand(const SessionItem* item, SessionItem* parent, TagRow tagrow)
+    : AbstractItemCommand(parent), p_impl(std::make_unique<CopyItemCommandPrivate>(tagrow))
 {
-    setDescription(generate_description(item->modelType(), p_impl->m_tagrow));
-    p_impl->m_backup_strategy = parent->model()->itemBackupStrategy();
-    p_impl->m_item_path = pathFromItem(parent);
+    setDescription(generate_description(item->modelType(), p_impl->tagrow));
+    p_impl->backup_strategy = parent->model()->itemBackupStrategy();
+    p_impl->item_path = pathFromItem(parent);
 
     auto copy_strategy = parent->model()->itemCopyStrategy(); // to modify id's
     auto item_copy = copy_strategy->createCopy(item);
 
-    p_impl->m_backup_strategy->saveItem(item_copy.get());
+    p_impl->backup_strategy->saveItem(item_copy.get());
 }
 
 CopyItemCommand::~CopyItemCommand() = default;
 
 void CopyItemCommand::undo_command()
 {
-    auto parent = itemFromPath(p_impl->m_item_path);
-    delete parent->takeItem(p_impl->m_tagrow);
-    p_impl->m_result = nullptr;
+    auto parent = itemFromPath(p_impl->item_path);
+    delete parent->takeItem(p_impl->tagrow);
+    p_impl->result = nullptr;
 }
 
 void CopyItemCommand::execute_command()
 {
-    auto parent = itemFromPath(p_impl->m_item_path);
-    auto item = p_impl->m_backup_strategy->restoreItem();
-    bool success = parent->insertItem(item.get(), p_impl->m_tagrow);
-    p_impl->m_result = success ? item.release() : nullptr;
+    auto parent = itemFromPath(p_impl->item_path);
+    auto item = p_impl->backup_strategy->restoreItem();
+    bool success = parent->insertItem(item.get(), p_impl->tagrow);
+    p_impl->result = success ? item.release() : nullptr;
 }
 
 CopyItemCommand::result_t CopyItemCommand::result() const
 {
-    return p_impl->m_result;
+    return p_impl->result;
 }
 
 namespace
