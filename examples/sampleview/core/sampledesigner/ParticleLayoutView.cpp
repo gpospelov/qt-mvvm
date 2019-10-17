a0e6d964d058cac5b2f36759da30b4b52ab2ff7e
Dmitry Yurov <d.yurov@fz-juelich.de>
2019-10-17 14:15:25 +0200

    Remove redundant interference function enumerators

--- examples/sampleview/core/sampledesigner/ParticleLayoutView.cpp
+++ examples/sampleview/core/sampledesigner/ParticleLayoutView.cpp
@@ -35,12 +35,7 @@ void ParticleLayoutView::addView(IView* childView)
 {
     if (childView->type() == DesignerHelper::PARTICLE) {
         connectInputPort(dynamic_cast<ConnectableView*>(childView), 0);
-    } else if (childView->type() == DesignerHelper::INTERFERENCE_FUNCTION_1D_LATTICE
-               || childView->type() == DesignerHelper::INTERFERENCE_FUNCTION_2D_LATTICE
-               || childView->type() == DesignerHelper::INTERFERENCE_FUNCTION_2D_PARA
-               || childView->type() == DesignerHelper::INTERFERENCE_FUNCTION_FINITE_2D_LATTICE
-               || childView->type() == DesignerHelper::INTERFERENCE_FUNCTION_HARD_DISK
-               || childView->type() == DesignerHelper::INTERFERENCE_FUNCTION_RADIAL_PARA) {
+    } else if (childView->type() == DesignerHelper::INTERFERENCE_FUNCTION) {
         connectInputPort(dynamic_cast<ConnectableView*>(childView), 1);
     } else {
         throw std::runtime_error("ParticleLayoutView::addView() -> Error. Unknown view");
