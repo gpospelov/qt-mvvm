787c139b3a77c67ef0c65d8154f1c23e8bf11f68
Dmitry Yurov <d.yurov@fz-juelich.de>
2019-10-18 14:00:39 +0200

    Fancier implementation of SampleViewFactory

--- examples/sampleview/core/sampledesigner/SampleViewFactory.cpp
+++ examples/sampleview/core/sampledesigner/SampleViewFactory.cpp
@@ -13,28 +13,28 @@
 #include "MultiLayerView.h"
 #include "ParticleLayoutView.h"
 #include "item_constants.h"
+#include <unordered_map>
 
 namespace {
-const QList<std::string> m_valid_item_names = {Constants::MultiLayerType, Constants::LayerType,
-                                               Constants::ParticleLayoutType,
-                                               Constants::InterferenceFunctionType};
+template <class T> IView* factory()
+{
+    return new T;
 }
+using FactoryFunc = IView*(*)();
+
+const std::unordered_map<std::string, FactoryFunc> item_map{
+    {Constants::MultiLayerType, factory<MultiLayerView>},
+    {Constants::LayerType, factory<LayerView>},
+    {Constants::ParticleLayoutType, factory<ParticleLayoutView>},
+    {Constants::InterferenceFunctionType, factory<InterferenceFunctionView>}};
+};
 
 bool SampleViewFactory::isValidType(const std::string& name)
 {
-    return m_valid_item_names.contains(name);
+    return item_map.find(name) != item_map.end();
 }
-
 
 IView* SampleViewFactory::createSampleView(const std::string& name)
 {
-    if (name == Constants::MultiLayerType)
-        return new MultiLayerView();
-    else if (name == Constants::LayerType)
-        return new LayerView();
-    else if (name == Constants::ParticleLayoutType)
-        return new ParticleLayoutView();
-    else if (name == Constants::InterferenceFunctionType)
-        return new InterferenceFunctionView();
-    return nullptr;
+    return isValidType(name) ? item_map.at(name)() : nullptr;
 }
