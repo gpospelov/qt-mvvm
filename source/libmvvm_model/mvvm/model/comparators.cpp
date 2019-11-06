92322511e6c201be0f4fd40e393fb5bd020bc006
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-11-06 16:15:58 +0100

    Fix missed top file headers

--- source/libmvvm_model/mvvm/model/comparators.cpp
+++ source/libmvvm_model/mvvm/model/comparators.cpp
@@ -1,3 +1,12 @@
+// ************************************************************************** //
+//
+//  Prototype of mini MVVM framework for bornagainproject.org
+//
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher
+//
+// ************************************************************************** //
+
 #include <mvvm/model/comparators.h>
 #include <mvvm/model/customvariants.h>
 #include <mvvm/model/comboproperty.h>
---
+++ source/libmvvm_model/mvvm/utils/containerutils.cpp
@@ -0,0 +1,10 @@
+// ************************************************************************** //
+//
+//  Prototype of mini MVVM framework for bornagainproject.org
+//
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher
+//
+// ************************************************************************** //
+
+#include "containerutils.h"
