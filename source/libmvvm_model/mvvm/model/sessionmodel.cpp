37fafbe778ad5ea140456cff67d4298213082b7f
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-11-14 16:41:19 +0100

    Provide isObsolete behavior for CopyItemCommand

--- source/libmvvm_model/mvvm/model/sessionmodel.cpp
+++ source/libmvvm_model/mvvm/model/sessionmodel.cpp
@@ -59,6 +59,7 @@ std::string SessionModel::modelType() const
 SessionItem* SessionModel::insertNewItem(const model_type& modelType, SessionItem* parent,
                                          const std::string& tag, int row)
 {
+    // intentionally passing by value inside lambda
     auto create_func = [this, modelType]() { return factory()->createItem(modelType); };
     return intern_insert(create_func, parent, tag, row);
 }
--- tests/model/copyitemcommand.test.cpp
+++ tests/model/copyitemcommand.test.cpp
@@ -13,6 +13,7 @@
 #include <mvvm/model/sessionitem.h>
 #include <mvvm/model/sessionmodel.h>
 #include <mvvm/model/taginfo.h>
+#include <mvvm/model/compounditem.h>
 
 using namespace ModelView;
 
@@ -43,6 +44,7 @@ TEST_F(CopyItemCommandTest, copyChild)
 
     // checking that parent has now three children
     auto copy = command->result();
+    EXPECT_FALSE(command->isObsolete());
     EXPECT_TRUE(copy != nullptr);
     EXPECT_EQ(parent->childrenCount(), 3);
     std::vector<SessionItem*> expected = {child0, copy, child1};
@@ -53,4 +55,31 @@ TEST_F(CopyItemCommandTest, copyChild)
     command->undo();
     expected = {child0, child1};
     EXPECT_EQ(parent->getItems("tag1"), expected);
+    EXPECT_FALSE(command->isObsolete());
+}
+
+//! Attempt to copy item to invalid tag
+
+TEST_F(CopyItemCommandTest, invalidCopyAttempt)
+{
+    SessionModel model;
+
+    // parent with children and data
+    auto parent = model.insertItem<CompoundItem>(model.rootItem(), "", 0);
+    parent->addProperty("thickness", 42.0);
+    parent->registerTag(TagInfo::universalTag("tag1"), /*set_as_default*/ true);
+
+    auto child0 = model.insertItem<SessionItem>(parent, "tag1", -1);
+    child0->setData(42.0);
+
+    // making copy of child
+    auto command = std::make_unique<CopyItemCommand>(child0, parent, TagRow{"thickness", 0});
+    command->execute();
+
+    // checking that parent has now three children
+    EXPECT_TRUE(command->isObsolete());
+    EXPECT_EQ(command->result(), nullptr);
+
+    // undoing of obsolete command is not possible
+    EXPECT_THROW(command->undo(), std::runtime_error);
 }
