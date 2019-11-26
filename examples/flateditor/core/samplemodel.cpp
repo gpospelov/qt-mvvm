f0424efe67d8f1f9f5b2b4035d06ef58c20c4000
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-11-26 13:45:13 +0100

    Placeholders for PropertyFlatView demo

---
+++ examples/flateditor/core/samplemodel.cpp
@@ -0,0 +1,50 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#include "samplemodel.h"
+#include <mvvm/model/itemcatalogue.h>
+#include "items.h"
+
+using namespace ModelView;
+
+namespace
+{
+std::unique_ptr<ModelView::ItemCatalogue> CreateToyItemCatalogue()
+{
+    auto result = std::make_unique<ItemCatalogue>();
+    result->registerItem<MultiLayer>();
+    result->registerItem<LayerItem>();
+    result->registerItem<ParticleItem>();
+    result->registerItem<InterferenceFunctionItem>();
+    result->registerItem<SphereItem>();
+    result->registerItem<CylinderItem>();
+    result->registerItem<AnysoPyramidItem>();
+    result->registerItem<ShapeGroupItem>();
+    return result;
+}
+} // namespace
+
+SampleModel::SampleModel() : SessionModel("SampleModel")
+{
+    setItemCatalogue(CreateToyItemCatalogue());
+    init_model();
+}
+
+//! Provides initial model content.
+
+void SampleModel::init_model()
+{
+    auto multi_layer = insertItem<MultiLayer>();
+    auto layer = insertItem<LayerItem>(multi_layer);
+    insertItem<ParticleItem>(layer);
+
+    insertItem<LayerItem>(multi_layer);
+
+    insertItem<InterferenceFunctionItem>();
+}
---
+++ examples/flateditor/core/samplemodel.h
@@ -0,0 +1,29 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#ifndef SAMPLEMODEL_H
+#define SAMPLEMODEL_H
+
+#include <mvvm/model/sessionmodel.h>
+
+/*!
+@class SampleModel
+@brief Main application model.
+*/
+
+class SampleModel : public ModelView::SessionModel
+{
+public:
+    SampleModel();
+
+private:
+    void init_model();
+};
+
+#endif
