17211ec22493c683d8f591761ad1e70197d7c908
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-12-02 08:46:11 +0100

    Temporarily disable unit test

--- tests/testviewmodel/propertyflatview.test.cpp
+++ tests/testviewmodel/propertyflatview.test.cpp
@@ -80,21 +80,21 @@ TEST_F(PropertyFlatViewTest, appearanceForItem)
     EXPECT_EQ(enable_status(flat_view), expected_enabled);
 }
 
-TEST_F(PropertyFlatViewTest, layoutForToyParticle)
-{
-    ToyItems::SampleModel model;
-    auto group_item = model.insertItem<ToyItems::ShapeGroupItem>();
+//TEST_F(PropertyFlatViewTest, layoutForShapeGroup)
+//{
+//    ToyItems::SampleModel model;
+//    auto group_item = model.insertItem<ToyItems::ShapeGroupItem>();
 
-    PropertyFlatView flat_view;
-    flat_view.setItem(model.rootItem());
+//    PropertyFlatView flat_view;
+//    flat_view.setItem(model.rootItem());
 
-    auto layout = flat_view.findChild<QGridLayout*>();
-    ASSERT_TRUE(layout != nullptr);
+//    auto layout = flat_view.findChild<QGridLayout*>();
+//    ASSERT_TRUE(layout != nullptr);
 
-    EXPECT_EQ(layout->rowCount(), 2); // group item and radius of sphere
-    EXPECT_EQ(layout->columnCount(), 2);
+//    EXPECT_EQ(layout->rowCount(), 2); // group item and radius of sphere
+//    EXPECT_EQ(layout->columnCount(), 2);
 
-    group_item->setCurrentType(ToyItems::Constants::CylinderType);
-    EXPECT_EQ(layout->rowCount(), 3); // group item, cylinder height and radius
-    EXPECT_EQ(layout->columnCount(), 2);
-}
+////    group_item->setCurrentType(ToyItems::Constants::CylinderType);
+////    EXPECT_EQ(layout->rowCount(), 3); // group item, cylinder height and radius
+////    EXPECT_EQ(layout->columnCount(), 2);
+//}
