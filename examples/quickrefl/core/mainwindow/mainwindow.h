951c9bb85daa4e0c22e69a398610b88ecc54f97c
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-01-17 10:27:01 +0100

    Implement placeholders for the prototype of reflectometry GUI

---
+++ examples/quickrefl/core/mainwindow/mainwindow.h
@@ -0,0 +1,38 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <memory>
+
+class QTabWidget;
+
+//! Application main window.
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow();
+    ~MainWindow();
+
+protected:
+    void closeEvent(QCloseEvent* event);
+
+private:
+    void init_application();
+    void write_settings();
+    void create_menus();
+
+    QTabWidget* m_tabWidget;
+};
+
+#endif //  MAINWINDOW_H
