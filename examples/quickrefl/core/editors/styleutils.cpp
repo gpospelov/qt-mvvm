5556bf0cb6b68cb206dd1cc16639ac8f1a0dbe0a
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-01-20 17:54:36 +0100

    Introduce common namespace for all style/high-dpi related settings

---
+++ examples/quickrefl/core/editors/styleutils.cpp
@@ -0,0 +1,16 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#include "styleutils.h"
+#include <QSize>
+
+QSize StyleUtils::ToolBarIconSize()
+{
+    return QSize(32, 32);
+}
---
+++ examples/quickrefl/core/editors/styleutils.h
@@ -0,0 +1,23 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#ifndef STYLEUTILS_H
+#define STYLEUTILS_H
+
+class QSize;
+
+//! Namespace for central access to all theme styling.
+
+namespace StyleUtils
+{
+    QSize ToolBarIconSize();
+};
+
+#endif // STYLEUTILS_H
+
