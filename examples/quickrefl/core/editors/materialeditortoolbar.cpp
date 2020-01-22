936c57d3e6ca4e0e56fd9c4c37a86f6d321ae5c5
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-01-22 17:12:29 +0100

    Add icon resources infrustructure to quickrefl example

--- examples/quickrefl/core/editors/materialeditortoolbar.cpp
+++ examples/quickrefl/core/editors/materialeditortoolbar.cpp
@@ -9,13 +9,37 @@
 
 #include "materialeditortoolbar.h"
 #include "styleutils.h"
+#include "resources.h"
+#include <QToolButton>
+#include <QAction>
+
 
 MaterialEditorToolBar::MaterialEditorToolBar(MaterialEditorActions* actions, QWidget* parent)
     : QToolBar(parent)
 {
+    InitIconResources();
+
+    setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
     setIconSize(StyleUtils::ToolBarIconSize());
 
-    addAction("Add material");
-    addAction("Clone");
-    addAction("Remove");
+    auto t0 = new QToolButton;
+    t0->setIcon(QIcon(":/icons/toolbar16dark_newitem.svg"));
+    t0->setText("Add material");
+    addWidget(t0);
+
+    auto t1 = new QToolButton;
+    t1->setIcon(QIcon(":/icons/toolbar16dark_newitem.svg"));
+    t1->setText("Add material");
+    addWidget(t1);
+
+    auto t2 = new QToolButton;
+    t2->setIcon(QIcon(":/icons/toolbar16dark_newitem.svg"));
+    t2->setText("Add material");
+    addWidget(t2);
+
+    auto t3 = new QAction;
+    t3->setIcon(QIcon(":/icons/toolbar16dark_newitem.svg"));
+    t3->setText("Add material");
+    addAction(t3);
+
 }
