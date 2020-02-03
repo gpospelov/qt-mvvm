0cec83a232a6e097bdef3baccb612a372b092289
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-02-03 16:06:17 +0100

    Refactor unit test to isolate test data creation

--- examples/quickrefl/tests/layereditoractions.test.cpp
+++ examples/quickrefl/tests/layereditoractions.test.cpp
@@ -24,41 +24,62 @@ class LayerEditorActionsTest : public ::testing::Test
 {
 public:
     ~LayerEditorActionsTest();
+
+    struct TestData {
+        SampleModel sample_model;
+        LayerViewModel view_model{&sample_model};
+        LayerSelectionModel selection_model{&view_model};
+        LayerEditorActions actions{&sample_model};
+        SessionItem* multilayer{nullptr};
+        SessionItem* top{nullptr};
+        SessionItem* bottom{nullptr};
+        TestData()
+        {
+            actions.setSelectionModel(&selection_model);
+            multilayer = sample_model.insertItem<MultiLayerItem>();
+            top = sample_model.insertItem<LayerItem>(multilayer);
+            bottom = sample_model.insertItem<LayerItem>(multilayer);
+        }
+    };
 };
 
 LayerEditorActionsTest::~LayerEditorActionsTest() = default;
+
+//! Checking initial data for testing.
+
+TEST_F(LayerEditorActionsTest, initialState)
+{
+    TestData test_data;
+
+    // checking layout of multilayer
+    auto layers = test_data.multilayer->getItems(MultiLayerItem::T_LAYERS);
+    EXPECT_EQ(layers.size(), 2);
+    EXPECT_EQ(layers.at(0), test_data.top);
+    EXPECT_EQ(layers.at(1), test_data.bottom);
+
+    // checking, that there are no layers selected
+    EXPECT_EQ(test_data.selection_model.selectedItems(), std::vector<SessionItem*>());
+}
 
 //! Adds new layer after selected layer.
 
 TEST_F(LayerEditorActionsTest, addNewLayerAfterSelection)
 {
-    // creating multi-layer with two layers
-    SampleModel model;
-    auto multilayer = model.insertItem<MultiLayerItem>();
-    auto top = model.insertItem<LayerItem>(multilayer);
-    auto bottom = model.insertItem<LayerItem>(multilayer);
-
-    // creating viewmodel looking at multi-layer
-    LayerViewModel viewmodel(&model);
-    viewmodel.setRootSessionItem(multilayer);
+    TestData test_data;
 
     // selecting top layer
-    LayerSelectionModel selection_model(&viewmodel);
-    selection_model.selectItem(top);
-
-    // initializing actions.
-    LayerEditorActions actions(&model);
-    actions.setSelectionModel(&selection_model);
+    test_data.selection_model.selectItem(test_data.top);
 
     // adding new layer after selection
-    actions.onAddLayer();
+    test_data.actions.onAddLayer();
 
-    auto layers = multilayer->getItems(MultiLayerItem::T_LAYERS);
+    // checking layout of multilayer
+    auto layers = test_data.multilayer->getItems(MultiLayerItem::T_LAYERS);
     EXPECT_EQ(layers.size(), 3);
-    EXPECT_EQ(layers.at(0), top);
-    EXPECT_EQ(layers.at(2), bottom);
+    EXPECT_EQ(layers.at(0), test_data.top);
+    EXPECT_EQ(layers.at(2), test_data.bottom);
 
     // checking, that new layer is selected
-    std::vector<SessionItem*> selected = {layers.at(1)};
-    EXPECT_EQ(selection_model.selectedItems(), selected);
+    std::vector<SessionItem*> expected = {layers.at(1)};
+    EXPECT_EQ(test_data.selection_model.selectedItems(), expected);
 }
