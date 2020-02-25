3dcd5d1a101ccbe277a92501517578e3344663e7
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-02-25 11:35:41 +0100

    Adapt tests to new model conventions in the part of default column count

--- tests/testviewmodel/propertyviewmodel.test.cpp
+++ tests/testviewmodel/propertyviewmodel.test.cpp
@@ -33,7 +33,7 @@ TEST_F(PropertyViewModelTest, initialState)
     PropertyViewModel viewModel(&model);
     EXPECT_EQ(viewModel.rowCount(), 0);
     EXPECT_EQ(viewModel.columnCount(), 0);
-    EXPECT_EQ(viewModel.sessionItemFromIndex(QModelIndex()), nullptr);
+    EXPECT_EQ(viewModel.sessionItemFromIndex(QModelIndex()), model.rootItem());
 }
 
 TEST_F(PropertyViewModelTest, baseItem)
@@ -46,7 +46,7 @@ TEST_F(PropertyViewModelTest, baseItem)
     // Root item has default tag and all items considered as top items.
     // PropertyViewModel shouldn't see any items.
     EXPECT_EQ(viewModel.rowCount(), 0);
-    EXPECT_EQ(viewModel.columnCount(), 2);
+    EXPECT_EQ(viewModel.columnCount(), 0);
 }
 
 TEST_F(PropertyViewModelTest, propertyItem)
@@ -79,7 +79,7 @@ TEST_F(PropertyViewModelTest, vectorItem)
     PropertyViewModel viewModel(&model);
 
     EXPECT_EQ(viewModel.rowCount(), 0); // root item doesn't have properties
-    EXPECT_EQ(viewModel.columnCount(), 2);
+    EXPECT_EQ(viewModel.columnCount(), 0);
 
     // switching to vectorItem and checking that it has 3 properties
     viewModel.setRootSessionItem(parent);
