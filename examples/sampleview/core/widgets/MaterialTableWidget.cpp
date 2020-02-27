105b9ed5d6f9c019fd00f79d2b07c41c25c339b4
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-02-27 16:20:37 +0100

    Restore MaterialTable in the example

--- examples/sampleview/core/widgets/MaterialTableWidget.cpp
+++ examples/sampleview/core/widgets/MaterialTableWidget.cpp
@@ -11,20 +11,30 @@
 #include "MaterialControlPanel.h"
 #include "MaterialModel.h"
 #include "MaterialTableView.h"
-#include <mvvm/viewmodel/defaultcelldecorator.h>
+#include "MaterialItems.h"
 #include <QHeaderView>
 #include <QVBoxLayout>
+#include <mvvm/viewmodel/defaultcelldecorator.h>
+#include <mvvm/viewmodel/standardviewmodels.h>
+#include <mvvm/viewmodel/viewmodeldelegate.h>
+#include <mvvm/model/modelutils.h>
 
 MaterialTableWidget::MaterialTableWidget(MaterialModel* material_model, QWidget* parent)
-    : QWidget(parent),
-      m_material_table(new QTableView)
+    : QWidget(parent), m_material_table(new QTableView),
+      view_model(ModelView::Utils::CreatePropertyTableViewModel(material_model)),
+      m_delegate(std::make_unique<ModelView::ViewModelDelegate>())
+
 {
     auto layout = new QVBoxLayout(this);
     layout->setMargin(0);
     layout->setSpacing(0);
 
     layout->addWidget(m_material_table);
+
+    view_model->setRootSessionItem(
+        ModelView::Utils::TopItem<MaterialContainerItem>(material_model));
+    m_material_table->setModel(view_model.get());
+    m_material_table->setItemDelegate(m_delegate.get());
 }
 
 MaterialTableWidget::~MaterialTableWidget() = default;
-
--- examples/sampleview/core/widgets/MaterialTableWidget.h
+++ examples/sampleview/core/widgets/MaterialTableWidget.h
@@ -13,6 +13,12 @@
 #include <QWidget>
 #include <memory>
 
+namespace ModelView
+{
+class ViewModel;
+class ViewModelDelegate;
+} // namespace ModelView
+
 class QTableView;
 class MaterialModel;
 class QStyledItemDelegate;
@@ -30,6 +36,8 @@ public:
 
 private:
     QTableView* m_material_table;
+    std::unique_ptr<ModelView::ViewModel> view_model;
+    std::unique_ptr<ModelView::ViewModelDelegate> m_delegate;
 };
 
 #endif // MATERIALTABLEWIDGET_H
