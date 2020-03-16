5ccc791301d53202727d66d26f66cb89ea4dc06a
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-03-16 20:37:11 +0100

    Implement ProgressHandler to maintain information about computation progress

---
+++ source/libmvvm_model/mvvm/utils/progresshandler.h
@@ -0,0 +1,54 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#ifndef PROGRESSHANDLER_H
+#define PROGRESSHANDLER_H
+
+#include <mvvm/core/export.h>
+#include <mutex>
+#include <functional>
+
+namespace ModelView
+{
+
+//! Maintain information about progress of a computation.
+//! Initialized with callback function to report progress and retrieve interruption request status.
+
+class ProgressHandler
+{
+public:
+    using callback_t = std::function<bool(size_t)>;
+
+    ProgressHandler() = default;
+    ProgressHandler(callback_t callback, size_t max_ticks_count);
+
+    ProgressHandler(const ProgressHandler& other) = delete;
+    ProgressHandler& operator=(const ProgressHandler& other) = delete;
+
+    void subscribe(callback_t callback);
+
+    void setMaxTicksCount(size_t value);
+
+    bool has_interrupt_request() const;
+
+    void setCompletedTicks(size_t value);
+
+    void reset();
+
+private:
+    std::mutex mutex;
+    callback_t runner_callback;
+    size_t max_ticks_count{0};
+    size_t completed_ticks{0};
+    bool interrupt_request{false};
+};
+
+} // namespace ModelView
+
+#endif
