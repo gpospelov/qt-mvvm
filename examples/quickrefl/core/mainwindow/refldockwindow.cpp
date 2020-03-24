4fd8b429bf8e9aecd462d1ff4d7c9d96d02a516e
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-03-24 18:08:59 +0100

    Introduce temporary red-color central widget for better judgment on dock placement

--- examples/quickrefl/core/mainwindow/refldockwindow.cpp
+++ examples/quickrefl/core/mainwindow/refldockwindow.cpp
@@ -13,22 +13,41 @@
 #include "dockscontroller.h"
 #include "layereditor.h"
 #include "reflwidgets.h"
+#include <QToolBar>
 #include <QLabel>
 
 ReflDockWindow::ReflDockWindow(QWidget* parent)
-    : QMainWindow(parent), docks_controller(new DocksController(this)),
+    : QMainWindow(parent), toolbar(new QToolBar), docks_controller(new DocksController(this)),
       models(std::make_unique<ApplicationModels>())
 {
-    setCentralWidget(new QLabel("Reflectometry docks"));
+    setup_toolbar();
+    setup_centralwidget();
 
     docks_controller->addWidget(0, new MaterialEditor(models->materialModel()),
                                 Qt::TopDockWidgetArea);
     docks_controller->addWidget(1, new LayerEditor(models.get()), Qt::TopDockWidgetArea);
-    docks_controller->addWidget(2, new LayerCanvas, Qt::LeftDockWidgetArea);
-    docks_controller->addWidget(3, new SLDViewWidget(models->sldController(), parent),
-                                Qt::RightDockWidgetArea);
-    docks_controller->addWidget(4, new ReflPlotWidget, Qt::BottomDockWidgetArea);
-    docks_controller->addWidget(5, new InstrumentEditor, Qt::BottomDockWidgetArea);
+    docks_controller->addWidget(3, new ReflPlotWidget, Qt::BottomDockWidgetArea);
+    docks_controller->addWidget(4, new SLDViewWidget(models->sldController(), parent),
+                                Qt::BottomDockWidgetArea);
 }
 
 ReflDockWindow::~ReflDockWindow() = default;
+
+void ReflDockWindow::setup_toolbar()
+{
+    const int toolbar_icon_size = 24;
+    toolbar->setIconSize(QSize(toolbar_icon_size, toolbar_icon_size));
+    addToolBar(toolbar);
+    //    setCentralWidget(toolbar);
+}
+
+//! Central widget as horizontal thin line.
+
+void ReflDockWindow::setup_centralwidget()
+{
+    auto central_widget = new QWidget;
+    central_widget->setStyleSheet("background-color:red;");
+    central_widget->setMinimumHeight(2);
+    central_widget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    setCentralWidget(central_widget);
+}
