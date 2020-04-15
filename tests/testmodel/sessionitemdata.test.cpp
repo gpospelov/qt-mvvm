c9e416d429029681cd0f758244cbf81f273ae9a0
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-04-15 18:10:38 +0200

    Implement SessionItemData::hasRole

--- tests/testmodel/sessionitemdata.test.cpp
+++ tests/testmodel/sessionitemdata.test.cpp
@@ -149,3 +149,18 @@ TEST_F(SessionItemDataTest, rangeLoop)
     EXPECT_EQ(values, expected_values);
     EXPECT_EQ(roles, expected_roles);
 }
+
+TEST_F(SessionItemDataTest, hasRole)
+{
+    SessionItemData data;
+    EXPECT_FALSE(data.hasRole(0));
+    EXPECT_FALSE(data.hasRole(1));
+
+    const int role = 99;
+    data.setData(QVariant::fromValue(42), role);
+    EXPECT_TRUE(data.hasRole(role));
+    EXPECT_FALSE(data.hasRole(1));
+
+    data.setData(QVariant(), role);
+    EXPECT_FALSE(data.hasRole(role));
+}
