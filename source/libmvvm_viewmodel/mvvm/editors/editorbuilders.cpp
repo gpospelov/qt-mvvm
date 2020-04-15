b3109426bfd486bbc77f85135e28c905af313bf4
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-04-15 18:30:06 +0200

    Simplify access to RealLimits in editor builders.

--- source/libmvvm_viewmodel/mvvm/editors/editorbuilders.cpp
+++ source/libmvvm_viewmodel/mvvm/editors/editorbuilders.cpp
@@ -52,10 +52,8 @@ builder_t IntegerEditorBuilder()
 {
     auto builder = [](const SessionItem* item) -> std::unique_ptr<CustomEditor> {
         auto editor = std::make_unique<IntegerEditor>();
-        auto variant = item->data<QVariant>(ItemDataRole::LIMITS);
-        // FIXME cleanup
-        if (variant.isValid()) {
-            auto limits = variant.value<RealLimits>();
+        if (item->hasData(ItemDataRole::LIMITS)) {
+            auto limits = item->data<RealLimits>();
             editor->setRange(static_cast<int>(limits.lowerLimit()),
                              static_cast<int>(limits.upperLimit()));
         }
@@ -68,10 +66,8 @@ builder_t DoubleEditorBuilder()
 {
     auto builder = [](const SessionItem* item) -> std::unique_ptr<CustomEditor> {
         auto editor = std::make_unique<DoubleEditor>();
-        auto variant = item->data<QVariant>(ItemDataRole::LIMITS);
-        // FIXME cleanup
-        if (variant.isValid()) {
-            auto limits = variant.value<RealLimits>();
+        if (item->hasData(ItemDataRole::LIMITS)) {
+            auto limits = item->data<RealLimits>();
             editor->setRange(limits.lowerLimit(), limits.upperLimit());
             editor->setSingleStep(singleStep(default_decimals));
             editor->setDecimals(default_decimals);
@@ -85,10 +81,8 @@ builder_t ScientificDoubleEditorBuilder()
 {
     auto builder = [](const SessionItem* item) -> std::unique_ptr<CustomEditor> {
         auto editor = std::make_unique<ScientificDoubleEditor>();
-        auto variant = item->data<QVariant>(ItemDataRole::LIMITS);
-        // FIXME cleanup
-        if (variant.isValid()) {
-            auto limits = variant.value<RealLimits>();
+        if (item->hasData(ItemDataRole::LIMITS)) {
+            auto limits = item->data<RealLimits>();
             editor->setRange(limits.lowerLimit(), limits.upperLimit());
         }
         return std::move(editor);
@@ -100,10 +94,8 @@ builder_t ScientificSpinBoxEditorBuilder()
 {
     auto builder = [](const SessionItem* item) -> std::unique_ptr<CustomEditor> {
         auto editor = std::make_unique<ScientificSpinBoxEditor>();
-        auto variant = item->data<QVariant>(ItemDataRole::LIMITS);
-        // FIXME cleanup
-        if (variant.isValid()) {
-            auto limits = variant.value<RealLimits>();
+        if (item->hasData(ItemDataRole::LIMITS)) {
+            auto limits = item->data<RealLimits>();
             editor->setRange(limits.lowerLimit(), limits.upperLimit());
         }
         editor->setSingleStep(getStep(item->data<double>()));
