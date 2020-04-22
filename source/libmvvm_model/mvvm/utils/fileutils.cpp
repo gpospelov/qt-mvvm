814754a20cafa94c556bc215eaf5991464450c0c
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-04-22 15:35:55 +0200

    Provide option to manually switch between filesystim and QDir

--- source/libmvvm_model/mvvm/utils/fileutils.cpp
+++ source/libmvvm_model/mvvm/utils/fileutils.cpp
@@ -8,42 +8,82 @@
 // ************************************************************************** //
 
 #include <QDir>
+#include <QFile>
 #include <QFileInfo>
 #include <mvvm/utils/fileutils.h>
-#include <mvvm/core/filesystem.h>
 #include <stdexcept>
+
+#ifndef DISABLE_FILESYSTEM
+#include <mvvm/core/filesystem.h>
+#endif
 
 using namespace ModelView;
 
 bool Utils::exists(const std::string& fileName)
 {
+#ifdef DISABLE_FILESYSTEM
     QFileInfo info(QString::fromStdString(fileName));
     return info.exists();
+#else
+    return std::filesystem::exists(fileName);
+#endif
 }
 
 std::string Utils::join(const std::string& part1, const std::string& part2)
 {
+#ifdef DISABLE_FILESYSTEM
+    return part1 + std::string("/") + part2;
+#else
     auto path = std::filesystem::path(part1) / std::filesystem::path(part2);
     return path.string();
+#endif
 }
 
 bool Utils::create_directory(const std::string& path)
 {
+#ifdef DISABLE_FILESYSTEM
+    QDir dir(QString::fromStdString(path));
+    return dir.mkpath(".");
+#else
     return std::filesystem::create_directory(path);
+#endif
 }
 
 bool Utils::remove(const std::string& path)
 {
+#ifdef DISABLE_FILESYSTEM
+    QFile file(QString::fromStdString(path));
+    return file.remove();
+#else
     return std::filesystem::remove(path);
+#endif
 }
 
 void Utils::remove_all(const std::string& path)
 {
+#ifdef DISABLE_FILESYSTEM
+    QDir dir(QString::fromStdString(path));
+    if (dir.exists())
+        dir.removeRecursively();
+#else
     std::filesystem::remove_all(path);
+#endif
 }
 
 std::vector<std::string> Utils::FindFiles(const std::string& dirname, const std::string& ext)
 {
+#ifdef DISABLE_FILESYSTEM
+    std::vector<std::string> result;
+    QDir dir(QString::fromStdString(dirname));
+    if (dir.exists()) {
+        QStringList filters = {QString::fromStdString("*" + ext)};
+        for (auto entry : dir.entryList(filters)) {
+            auto name = dir.filePath(entry);
+            result.push_back(name.toStdString());
+        }
+    }
+    return result;
+#else
     std::vector<std::string> result;
     for (const auto& entry : std::filesystem::directory_iterator(dirname)) {
         const auto filenameStr = entry.path().filename().string();
@@ -51,4 +91,5 @@ std::vector<std::string> Utils::FindFiles(const std::string& dirname, const std:
             result.push_back(entry.path().string());
     }
     return result;
+#endif
 }
