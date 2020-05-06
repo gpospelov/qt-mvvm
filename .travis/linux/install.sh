f98f8698fb6648b4d54f49caa8eb567aa6be8b68
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-05-06 11:26:03 +0200

    Require Qt5.14 in Travis builds

--- .travis/linux/install.sh
+++ .travis/linux/install.sh
@@ -1,6 +1,9 @@
 # install qt
-sudo apt-get update
-sudo apt-get install -y -qq --no-install-recommends qt5-default qtbase5-dev
+sudo apt-add-repository -y ppa:beineri/opt-qt-5.14.2-bionic
+sudo apt-get -qq update 
+sudo apt-get install qt514base qt514imageformats qt514svg  qt514tools -y
+sudo apt-get install libgl1-mesa-dev -y
+source /opt/qt514/bin/qt514-env.sh
 sudo apt-get -qq clean
 
 # install cmake
