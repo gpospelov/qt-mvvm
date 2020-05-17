394a0a2f9d23616cc36e704b96da58d99f0f45f0
AlexanderSchober <alex.schober@mac.com>
2020-05-17 07:35:53 +0200

    Provided tests for the ImportLogic class

---
+++ examples/quickrefl/tests/importfile.creator.h
@@ -0,0 +1,164 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#include "importutils.h"
+#include "test_utils.h"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <string>
+
+namespace CreateTestFiles
+{
+
+//! Generate the axis column
+std::vector<double> generateAxisColumn(int rows)
+{
+    std::vector<double> output(rows);
+    double gen_i = 0;
+    std::generate(output.begin(), output.end(),
+                  [&rows, &gen_i]() { return gen_i++ * (3 - 3e-1) / rows; });
+    return output;
+}
+
+//! Generate the data column
+std::vector<double> generateDataColumn(int rows)
+{
+    std::uniform_real_distribution<double> unif(1e2, 1e8);
+    std::default_random_engine re;
+
+    std::vector<double> output(rows);
+    std::generate(output.begin(), output.end(), [&re, &unif]() { return unif(re); });
+    return output;
+}
+
+//! Generate the data of number of columns
+std::vector<std::vector<double>> generateData(int column, int rows)
+{
+    std::vector<std::vector<double>> data;
+
+    if (column > 1)
+        data.push_back(generateAxisColumn(rows));
+
+    for (int i = 0; i < column - 1; ++i) {
+        data.push_back(generateDataColumn(rows));
+    }
+
+    return DataImportUtils::transpose(data);
+}
+
+//! Generate the header of number of columns
+std::map<std::string, int> generateHeader(int column)
+{
+    std::map<std::string, int> header;
+    for (int i = 0; i < column; ++i) {
+        header.insert(std::pair<std::string, int>(std::string{"Header_" + std::to_string(i)}, i));
+    }
+    return header;
+}
+
+//! Convert an input of doubles to sring
+std::vector<std::vector<std::string>> convertToString(std::vector<std::vector<double>> input)
+{
+    std::vector<std::vector<std::string>> output(input.size(),
+                                                 std::vector<std::string>(input[0].size(), ""));
+    for (int i = 0; i < input.size(); i++) {
+        for (int j = 0; j < input[0].size(); ++j) {
+            std::ostringstream ss;
+            ss << input[i][j];
+            output[i][j] = ss.str();
+        }
+    }
+    return output;
+}
+
+//! Simple structure for input passing
+struct FileInput {
+public:
+    std::string name;
+    std::string path;
+
+    char separator;
+    int col_num;
+    int row_num;
+
+    std::vector<std::vector<std::string>> data;
+    std::map<std::string, int> header;
+};
+
+//! Write a data file
+FileInput writeDataFile(const std::string& name, char separator, int columns = 4, int rows = 100)
+{
+    std::string test_file_subfolder = "importtestfiles";
+    std::string file_path;
+    TestUtils::CreateTestDirectory(test_file_subfolder);
+    file_path = TestUtils::TestFileName(test_file_subfolder, name);
+    remove(file_path.c_str());
+
+    // Prepare the buffer that will be used to compare test output
+    FileInput file_input;
+    file_input.name = name;
+    file_input.path = file_path;
+
+    file_input.separator = separator;
+    file_input.col_num = columns;
+    file_input.row_num = rows;
+
+    file_input.data = convertToString(generateData(columns, rows));
+    file_input.header = generateHeader(columns);
+
+    std::ofstream file(file_path);
+    // Write the header
+    {
+        std::string feed;
+        for (const auto& header_item : file_input.header) {
+            feed += header_item.first + separator;
+        }
+        feed.pop_back();
+        feed += "\n";
+        file << feed;
+    }
+
+    // Write the data
+    for (const auto& data_row : file_input.data) {
+        std::string feed;
+        for (const auto& data_item : data_row) {
+            feed += data_item + separator;
+        }
+        feed.pop_back();
+        feed += "\n";
+        file << feed;
+    }
+
+    file.close();
+    return file_input;
+}
+
+//! Quick create comma separator file
+FileInput createCommaCSVFile(const std::string& name)
+{
+    return writeDataFile(name, ',');
+}
+
+//! Quick create space separator file
+FileInput createSpaceCSVFile(const std::string& name)
+{
+    return writeDataFile(name, ' ');
+}
+
+//! Quick create semicolumn separator file
+FileInput createSemicolumnCSVFile(const std::string& name)
+{
+    return writeDataFile(name, ';');
+}
+
+} // End of namespace CreateTestFiles
---
+++ examples/quickrefl/tests/importlogic.test.cpp
@@ -0,0 +1,258 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#include "google_test.h"
+#include "test_utils.h"
+
+#include "importfile.creator.h"
+#include "importlinefilter.h"
+#include "importlogic.h"
+
+#include <cmath>
+#include <chrono>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace DataImportLogic;
+
+//! Test the data column item
+class ImportLogicTest : public ::testing::Test
+{
+public:
+    ImportLogicTest();
+    ~ImportLogicTest();
+
+    std::vector<CreateTestFiles::FileInput> generateFiles(char separator);
+    std::vector<std::string> getPaths(const std::vector<CreateTestFiles::FileInput>& inputs);
+    std::vector<double> stringToDouble(std::vector<std::string> input);
+
+    ImportLogic import_logic;
+    std::map<std::string, char> separators;
+};
+
+ImportLogicTest::ImportLogicTest() : ::testing::Test(), import_logic()
+{
+    // Set up the separators
+    separators.insert(std::pair<std::string, char>("Space ( )", ' '));
+    separators.insert(std::pair<std::string, char>("Comma (,)", ','));
+    separators.insert(std::pair<std::string, char>("Semicolon (;)", ';'));
+    separators.insert(std::pair<std::string, char>("Pipe (|)", '|'));
+    separators.insert(std::pair<std::string, char>("Colon (:)", ':'));
+    separators.insert(std::pair<std::string, char>("Tab", '\t'));
+    separators.insert(std::pair<std::string, char>("Quote (\')", '\''));
+    separators.insert(std::pair<std::string, char>("Apostrophe (\")", '\"'));
+}
+
+//! Convert the string vector to vector of double
+std::vector<double> ImportLogicTest::stringToDouble(std::vector<std::string> input)
+{
+    std::vector<double> output;
+    std::transform(input.begin(), input.end(), std::back_inserter(output),
+                   [](const std::string& val) { return std::stod(val); });
+    return output;
+}
+
+//! Produce 10 files of the given separator
+std::vector<CreateTestFiles::FileInput> ImportLogicTest::generateFiles(char separator)
+{
+    std::vector<CreateTestFiles::FileInput> file_inputs;
+    for (int i = 0; i < 10; ++i) {
+        std::string path = "file_" + std::to_string(std::chrono::duration_cast<std::chrono::nanoseconds>
+              (std::chrono::high_resolution_clock::now().time_since_epoch()).count()) + ".txt";
+        file_inputs.push_back(CreateTestFiles::writeDataFile(path, separator));
+    }
+    return file_inputs;
+}
+
+//! Extract the paths
+std::vector<std::string>
+ImportLogicTest::getPaths(const std::vector<CreateTestFiles::FileInput>& file_inputs)
+{
+    std::vector<std::string> path_vector;
+    std::transform(
+        file_inputs.begin(), file_inputs.end(), std::back_inserter(path_vector),
+        [](const CreateTestFiles::FileInput& input) -> std::string { return input.path; });
+    return path_vector;
+}
+
+ImportLogicTest::~ImportLogicTest() = default;
+
+//! Test default constructor
+TEST_F(ImportLogicTest, defaultContructor)
+{
+    CreateTestFiles::createSpaceCSVFile("SpaceTest.txt");
+}
+
+//! Test the add/remove filter
+TEST_F(ImportLogicTest, lineFilterAddRemoveTest)
+{
+    auto filter_0 = import_logic.addLineFilter("Header");
+    auto filter_1 = import_logic.addLineFilter("Data");
+    auto filter_2 = import_logic.addLineFilter("Unit");
+    auto filter_3 = import_logic.addLineFilter("Comment");
+
+    EXPECT_EQ(4, import_logic.lineFilterOrder().size());
+    import_logic.removeLineFilter(filter_1);
+    EXPECT_EQ(3, import_logic.lineFilterOrder().size());
+    import_logic.removeLineFilter(filter_2);
+    EXPECT_EQ(2, import_logic.lineFilterOrder().size());
+
+    filter_1 = import_logic.addLineFilter("Data");
+    filter_2 = import_logic.addLineFilter("Unit");
+    EXPECT_EQ(4, import_logic.lineFilterOrder().size());
+}
+
+//! Test the filter order
+TEST_F(ImportLogicTest, lineFilterOrderTest)
+{
+    auto filter_0 = import_logic.addLineFilter("Header");
+    auto filter_1 = import_logic.addLineFilter("Data");
+    auto filter_2 = import_logic.addLineFilter("Unit");
+    auto filter_3 = import_logic.addLineFilter("Comment");
+
+    std::vector<LineFilter*> order{filter_0, filter_1, filter_2, filter_3};
+    EXPECT_EQ(order, import_logic.lineFilterOrder());
+
+    std::vector<LineFilter*> new_order{filter_3, filter_1, filter_2, filter_0};
+    import_logic.setLineFilterOrder(new_order);
+    EXPECT_EQ(new_order, import_logic.lineFilterOrder());
+}
+
+//! Test that the hasName method yields the right result
+TEST_F(ImportLogicTest, hasNameTest)
+{
+    auto filter_0 = import_logic.addLineFilter("Header");
+    auto filter_1 = import_logic.addLineFilter("Data");
+    auto filter_2 = import_logic.addLineFilter("Unit");
+    auto filter_3 = import_logic.addLineFilter("Comment");
+
+    EXPECT_EQ(filter_0, import_logic.nameInFilters("Header"));
+    EXPECT_EQ(filter_1, import_logic.nameInFilters("Data"));
+    EXPECT_EQ(filter_2, import_logic.nameInFilters("Unit"));
+    EXPECT_EQ(filter_3, import_logic.nameInFilters("Comment"));
+
+    EXPECT_EQ(nullptr, import_logic.nameInFilters("Not_present"));
+}
+
+//! Test that the hasType method yields the right result
+TEST_F(ImportLogicTest, hasTypeTest)
+{
+    auto filter_0 = import_logic.addLineFilter("Header");
+    auto filter_1 = import_logic.addLineFilter("Data");
+    auto filter_2 = import_logic.addLineFilter("Unit");
+    auto filter_3 = import_logic.addLineFilter("Comment");
+
+    filter_0->setType("Header");
+    filter_1->setType("Data");
+    filter_2->setType("Unit");
+    filter_3->setType("Comment");
+
+    EXPECT_EQ(filter_0, import_logic.typeInFilters("Header"));
+    EXPECT_EQ(filter_1, import_logic.typeInFilters("Data"));
+    EXPECT_EQ(filter_2, import_logic.typeInFilters("Unit"));
+    EXPECT_EQ(filter_3, import_logic.typeInFilters("Comment"));
+
+    EXPECT_EQ(nullptr, import_logic.typeInFilters("Not_present_type"));
+}
+
+//! Test that set files puts the right amount of files into the container
+TEST_F(ImportLogicTest, setFilesTest)
+{
+    std::vector<CreateTestFiles::FileInput> file_inputs;
+    for (std::map<std::string, char>::iterator it = separators.begin(); it != separators.end();
+         ++it) {
+        file_inputs = generateFiles(it->second);
+        import_logic.setFiles(getPaths(file_inputs));
+        EXPECT_EQ(file_inputs.size(), import_logic.getFinalOutput().keys().size());
+    }
+}
+
+//! Test the header processing
+TEST_F(ImportLogicTest, lineFiltersHeaderTest)
+{
+    auto filter_0 = import_logic.addLineFilter("Header");
+
+    filter_0->setType("Header");
+    filter_0->setActive(true);
+
+    std::vector<CreateTestFiles::FileInput> file_inputs;
+    for (std::map<std::string, char>::iterator it = separators.begin(); it != separators.end();
+         ++it) {
+        file_inputs = generateFiles(it->second);
+        import_logic.setFiles(getPaths(file_inputs));
+        filter_0->setSeparator(it->first);
+
+        int i = 0;
+        for (const auto& file_input : file_inputs) {
+            EXPECT_EQ(file_input.header, import_logic.getHeader(i++));
+        }
+    }
+}
+
+//! Test the data processing
+TEST_F(ImportLogicTest, lineFiltersDataTest)
+{
+    auto filter_1 = import_logic.addLineFilter("Data");
+
+    filter_1->setType("Data");
+    filter_1->setActive(true);
+    filter_1->setStart(1);
+    filter_1->setEnd(-1);
+
+    std::vector<CreateTestFiles::FileInput> file_inputs;
+    for (std::map<std::string, char>::iterator it = separators.begin(); it != separators.end();
+         ++it) {
+        file_inputs = generateFiles(it->second);
+        import_logic.setFiles(getPaths(file_inputs));
+        filter_1->setSeparator(it->first);
+
+        int i = 0;
+        for (const auto& file_input : file_inputs) {
+            EXPECT_EQ(file_input.data, import_logic.getData(i++));
+        }
+    }
+}
+
+//! Test the data processing
+TEST_F(ImportLogicTest, totalFiltersResultTest)
+{
+    auto filter_0 = import_logic.addLineFilter("Header");
+    auto filter_1 = import_logic.addLineFilter("Data");
+
+    filter_0->setType("Header");
+    filter_0->setActive(true);
+
+    filter_1->setType("Data");
+    filter_1->setActive(true);
+    filter_1->setStart(1);
+    filter_1->setEnd(-1);
+
+    std::vector<CreateTestFiles::FileInput> file_inputs;
+    for (std::map<std::string, char>::iterator it = separators.begin(); it != separators.end();
+         ++it) {
+        file_inputs = generateFiles(it->second);
+        import_logic.setFiles(getPaths(file_inputs));
+        filter_0->setSeparator(it->first);
+        filter_1->setSeparator(it->first);
+        auto output_items = import_logic.getFinalOutput();
+
+        for (int i = 0; i < file_inputs.size(); ++i) {
+            auto output_item = output_items[file_inputs[i].path];
+            EXPECT_EQ(true, output_item->hasAxis());
+            auto axis = stringToDouble(DataImportUtils::transpose(file_inputs[i].data).at(0));
+            EXPECT_EQ(axis, output_item->axis());
+            for (int j = 0; j < output_item->dataCount(); ++j) {
+                auto data =
+                    stringToDouble(DataImportUtils::transpose(file_inputs[i].data).at(j + 1));
+                EXPECT_EQ(data, output_item->data(j));
+            }
+        }
+    }
+}
