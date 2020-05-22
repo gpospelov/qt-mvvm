10ee47b554aa9cd2550f3f98601994ed48549aba
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-05-22 09:16:43 +0200

    New folder for model-related interfaces. Move ModelListenerInterface to it.

--- source/libmvvm_model/mvvm/interfaces/modellistenerinterface.h
+++ source/libmvvm_model/mvvm/interfaces/modellistenerinterface.h
@@ -7,8 +7,8 @@
 //
 // ************************************************************************** //
 
-#ifndef MVVM_SIGNALS_MODELMAPPERINTERFACE_H
-#define MVVM_SIGNALS_MODELMAPPERINTERFACE_H
+#ifndef MVVM_INTERFACES_MODELLISTENERINTERFACE_H
+#define MVVM_INTERFACES_MODELLISTENERINTERFACE_H
 
 #include <mvvm/signals/callback_types.h>
 
@@ -27,9 +27,11 @@ public:
     virtual void setOnItemRemoved(Callbacks::item_tagrow_t f, Callbacks::slot_t client) = 0;
     virtual void setOnAboutToRemoveItem(Callbacks::item_tagrow_t f, Callbacks::slot_t client) = 0;
     virtual void setOnModelDestroyed(Callbacks::model_t f, Callbacks::slot_t client) = 0;
+    virtual void setOnModelAboutToBeReset(Callbacks::model_t f, Callbacks::slot_t client) = 0;
     virtual void setOnModelReset(Callbacks::model_t f, Callbacks::slot_t client) = 0;
+    virtual void unsubscribe(Callbacks::slot_t client) = 0;
 };
 
 } // namespace ModelView
 
-#endif // MVVM_SIGNALS_MODELMAPPERINTERFACE_H
+#endif // MVVM_INTERFACES_MODELLISTENERINTERFACE_H
