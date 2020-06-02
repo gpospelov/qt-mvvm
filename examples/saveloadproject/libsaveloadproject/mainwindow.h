2112fcef71bdbe604de63c9ed3b820aeafef3315
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-02 09:00:28 +0200

    Placeholder for new 'saveloadproject' example, demonstrating ProjectManager features

---
+++ examples/saveloadproject/libsaveloadproject/mainwindow.h
@@ -0,0 +1,33 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+//! Application main window.
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow();
+    ~MainWindow();
+
+protected:
+    void closeEvent(QCloseEvent* event);
+
+private:
+    void init_application();
+    void init_views();
+    void write_settings();
+};
+
+#endif //  MAINWINDOW_H
