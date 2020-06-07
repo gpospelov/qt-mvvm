504fabefb4608a0fc42c32668f6e38f9fd1683ca
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-07 15:55:22 +0200

    Switch to different qt5 repo at launchpad, switch to cmake 3.17

--- .travis/linux/install.sh
+++ .travis/linux/install.sh
@@ -1,4 +1,11 @@
-docker build -t bornagain/focal -f `dirname $0`/Dockerfile .
-docker run --security-opt seccomp=unconfined -d --name dd -it bornagain/focal
-docker ps -a
+# install qt
+sudo add-apt-repository ppa:ymshenyu/qt514
+sudo apt-get update
+sudo apt-get install qt5-default libqt5designercomponents5 qttools5-dev libqt5svg5-dev libqt5opengl5-dev libgl1-mesa-dev -y
 
+# install cmake
+wget --no-check-certificate --quiet https://cmake.org/files/v3.17/cmake-3.17.3-Linux-x86_64.tar.gz
+tar -zxf cmake-3.17.3-Linux-x86_64.tar.gz
+mv ./cmake-3.17.3-Linux-x86_64 ./${CMAKE_DIRNAME}
+rm cmake-3.17.3-Linux-x86_64.tar.gz
+cd ${TRAVIS_BUILD_DIR}
