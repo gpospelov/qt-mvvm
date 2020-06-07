6c901698c33d46bd21c57b3cd3bf79550663de82
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-07 16:19:30 +0200

    Move custom Qt5 setup to later step,switch to Qt from ppa:beineri Switch to different qt5 repo at launchpad, switch to cmake 3.17 Fix Linux travis config Fix dew Qt5.15 deprecated warnings

--- .travis/linux/build.sh
+++ .travis/linux/build.sh
@@ -1,3 +1,8 @@
-docker exec dd bash -c "mkdir build"
-docker exec dd bash -c "cmake -DMVVM_ENABLE_FILESYSTEM=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DMVVM_GTEST_DISCOVER_TESTS=$TESTS_AUTODISCOVERY .."
-docker exec dd bash -c "cd build && make -j4"
+# adding cmake to system path
+export PATH=${TRAVIS_BUILD_DIR}/${CMAKE_DIRNAME}:${TRAVIS_BUILD_DIR}/${CMAKE_DIRNAME}/bin:$PATH
+
+mkdir build
+cd build
+source /opt/qt514/bin/qt514-env.sh
+cmake -DMVVM_ENABLE_FILESYSTEM=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DMVVM_GTEST_DISCOVER_TESTS=$TESTS_AUTODISCOVERY ..
+make -j4
