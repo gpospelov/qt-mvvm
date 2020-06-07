4531cee906feec493908efdf7f9694612853a08b
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-07 10:44:15 +0200

    Fix Linux travis config

--- .travis/linux/build.sh
+++ .travis/linux/build.sh
@@ -1,3 +1,3 @@
 docker exec dd bash -c "mkdir build"
-docker exec dd bash -c "cmake -DMVVM_ENABLE_FILESYSTEM=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DMVVM_GTEST_DISCOVER_TESTS=$TESTS_AUTODISCOVERY .."
-docker exec dd bash -c "cd build && make -j4"
+docker exec dd bash -c "cd build && cmake -DMVVM_ENABLE_FILESYSTEM=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DMVVM_GTEST_DISCOVER_TESTS=$TESTS_AUTODISCOVERY .."
+docker exec dd bash -c "cd build && QT_DEBUG_PLUGINS=1 QT_QPA_PLATFORM=offscreen && make -j4"
