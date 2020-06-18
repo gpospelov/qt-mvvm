d2b4815935503a931e55d054b1ff1ce0982fbeac
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-18 16:29:12 +0200

    Add auto generation of version.h from CMake PROJECT_VERSION string

--- cmake/modules/configuration.cmake
+++ cmake/modules/configuration.cmake
@@ -25,6 +25,7 @@ message(STATUS " Qt5 Includes  : ${Qt5Widgets_INCLUDE_DIRS}")
 # Generating config files
 
 configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/testconfig.h.in  ${BUILD_INC_DIR}/testconfig.h @ONLY)
+configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/mvvm_version.h.in  ${BUILD_INC_DIR}/mvvm_version.h @ONLY)
 add_definitions(-I${BUILD_INC_DIR})
 
 add_compile_options($<$<CXX_COMPILER_ID:MSVC>:/MP>)
