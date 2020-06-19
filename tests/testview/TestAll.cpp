45ff32e8ab78e9308a37136328f76a73847fa934
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-19 15:51:35 +0200

    Split unit tests on two separate executable, one for libmvvm_viewmodel.so another for libmvvm_view.so

---
+++ tests/testview/TestAll.cpp
@@ -0,0 +1,29 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#include "google_test.h"
+#include "qcustomplot.h"
+#include <QApplication>
+#include <QStandardItem>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    ModelView::Comparators::registerComparators();
+    qRegisterMetaType<QStandardItem*>("QStandardItem*");
+    qRegisterMetaType<QCPRange>("QCPRange");
+
+    // FIXME find the way not to run app for all tests which doesn't use QWidget.
+    // The problem here is because of ctest autodiscovery which runs given main at every test.
+    QApplication app(argc, argv);
+    Q_UNUSED(app)
+
+    return RUN_ALL_TESTS();
+}
