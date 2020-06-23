a59f1f648f7505c38eefebd88ddf50d26b25d819
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-23 14:30:36 +0200

    Add installation of libmmvm_view library

--- cmake/modules/installation.cmake
+++ cmake/modules/installation.cmake
@@ -1,9 +1,10 @@
 # Installation and exporting
+# Thanks to https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
+# Thanks to https://gitlab.kitware.com/cmake/community/-/wikis/doc/tutorials/Exporting-and-Importing-Targets
 
-include(GNUInstallDirs)
 set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/mvvm)
 
-#Export the targets to a script
+# exporting targets to a script
 install(EXPORT mvvm-targets FILE MVVMTargets.cmake NAMESPACE MVVM:: DESTINATION ${INSTALL_CONFIGDIR})
 
 
