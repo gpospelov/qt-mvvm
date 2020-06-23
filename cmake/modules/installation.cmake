9f7c641639669e1ffc46cb712a75cee37fa31822
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-23 14:45:01 +0200

    Add instruction for packages export

--- cmake/modules/installation.cmake
+++ cmake/modules/installation.cmake
@@ -1,16 +1,20 @@
-# Installation and exporting
-# Thanks to https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
-# Thanks to https://gitlab.kitware.com/cmake/community/-/wikis/doc/tutorials/Exporting-and-Importing-Targets
+# -----------------------------------------------------------------------------
+# Installation
+# Credits to https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
+# Credits to https://gitlab.kitware.com/cmake/community/-/wikis/doc/tutorials/Exporting-and-Importing-Targets
+# -----------------------------------------------------------------------------
 
 set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/mvvm)
 
 # exporting targets to a script
 install(EXPORT mvvm-targets FILE MVVMTargets.cmake NAMESPACE MVVM:: DESTINATION ${INSTALL_CONFIGDIR})
 
+# -----------------------------------------------------------------------------
+# Exporting
+# -----------------------------------------------------------------------------
 
-# #############################################################################
-# Export
-# #############################################################################
+# export into build tree
+export(TARGETS mvvm_model mvvm_viewmodel mvvm_view NAMESPACE MVVM:: FILE MVVMTargets.cmake)
 
-#export(TARGETS mvvm_model mvvm_viewmodel- NAMESPACE MVVM:: FILE MVVMTargets.cmake)
-#export(PACKAGE MVVM)
+# export into $HOME/.cmake
+export(PACKAGE MVVM)
