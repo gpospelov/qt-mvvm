42862b10da4cef759513e731f424224410211352
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-23 15:19:32 +0200

    Generate and install package config

--- cmake/modules/installation.cmake
+++ cmake/modules/installation.cmake
@@ -1,20 +1,42 @@
 # -----------------------------------------------------------------------------
 # Installation
-# Credits to https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
-# Credits to https://gitlab.kitware.com/cmake/community/-/wikis/doc/tutorials/Exporting-and-Importing-Targets
+# Credits to https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right,
+# https://gitlab.kitware.com/cmake/community/-/wikis/doc/tutorials/Exporting-and-Importing-Targets
 # -----------------------------------------------------------------------------
 
 set(INSTALL_CONFIGDIR ${CMAKE_INSTALL_LIBDIR}/cmake/mvvm)
 
-# exporting targets to a script
+# exporting targets to a script and installing it
 install(EXPORT mvvm-targets FILE MVVMTargets.cmake NAMESPACE MVVM:: DESTINATION ${INSTALL_CONFIGDIR})
 
 # -----------------------------------------------------------------------------
 # Exporting
 # -----------------------------------------------------------------------------
 
-# export into build tree
+# Add all targets to the build-tree export set
 export(TARGETS mvvm_model mvvm_viewmodel mvvm_view NAMESPACE MVVM:: FILE MVVMTargets.cmake)
 
-# export into $HOME/.cmake
+# Export the package for use from the build-tree (goes to $HOME/.cmake)
 export(PACKAGE MVVM)
+
+# -----------------------------------------------------------------------------
+# Creating and installing MVVMConfig.cmake
+# -----------------------------------------------------------------------------
+
+include(CMakePackageConfigHelpers)
+
+configure_package_config_file(${CMAKE_SOURCE_DIR}/cmake/scripts/MVVMConfig.cmake.in
+    ${CMAKE_CURRENT_BINARY_DIR}/MVVMConfig.cmake
+    INSTALL_DESTINATION ${INSTALL_CONFIGDIR}
+)
+
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/MVVMConfig.cmake DESTINATION ${INSTALL_CONFIGDIR})
+
+# -----------------------------------------------------------------------------
+# Create and install ConfigVersion.cmake file
+# -----------------------------------------------------------------------------
+
+write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/MVVMConfigVersion.cmake VERSION
+    ${PROJECT_VERSION} COMPATIBILITY AnyNewerVersion)
+
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/MVVMConfigVersion.cmake DESTINATION ${INSTALL_CONFIGDIR})
