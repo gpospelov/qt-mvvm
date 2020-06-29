6467123f655f4d0ec3872aa3cc160988b58fb8f0
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-29 16:34:09 +0200

    Introduce aggregates to hold construction paramters for Project and for ProjectManager

--- source/libmvvm_model/mvvm/project/project_types.h
+++ source/libmvvm_model/mvvm/project/project_types.h
@@ -10,11 +10,45 @@
 #ifndef MVVM_PROJECT_PROJECT_TYPES_H
 #define MVVM_PROJECT_PROJECT_TYPES_H
 
+#include <functional>
+#include <string>
+#include <vector>
+
 namespace ModelView
 {
 
+class SessionModel;
+
 //! Possible user answers on question "Project was modified".
 enum class SaveChangesAnswer { SAVE = 0, DISCARD = 1, CANCEL = 2 };
+
+//! Provides necessary information for Project construction.
+
+struct ProjectContext {
+    //!< To notify about the change of the project with respect to what was written on disk.
+    using modified_callback_t = std::function<void()>;
+    //! To ask for a vector of models to save/load to/from disk.
+    using models_callback_t = std::function<std::vector<SessionModel*>()>;
+
+    modified_callback_t m_modified_callback;
+    models_callback_t m_models_callback;
+};
+
+//! Defines the context to interact with the user regarding save/save-as/create-new project
+//! scenarious.
+
+struct UserInteractionContext {
+    //!< To ask the user to select existing directory, returns full path to the directory.
+    using select_dir_callback_t = std::function<std::string()>;
+    //!< To ask the user to create a new directory, returns full path to the directory.
+    using create_dir_callback_t = std::function<std::string()>;
+    //!< To ask the user what to do with modified project.
+    using answer_callback_t = std::function<SaveChangesAnswer()>;
+
+    select_dir_callback_t m_select_dir_callback;
+    create_dir_callback_t m_create_dir_callback;
+    answer_callback_t m_answer_callback;
+};
 
 } // namespace ModelView
 
