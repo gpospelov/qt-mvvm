42741d58ab61cff16591922e92643bcd33819760
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-06-30 22:48:01 +0200

    Attempt to import qml module

---
+++ examples/modelinqml/modelinqmlcore/tablemodel.h
@@ -0,0 +1,38 @@
+#include <qqml.h>
+#include <QAbstractTableModel>
+
+
+class TableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_ADDED_IN_MINOR_VERSION(1)
+
+public:
+    int rowCount(const QModelIndex & = QModelIndex()) const override
+    {
+        return 200;
+    }
+
+    int columnCount(const QModelIndex & = QModelIndex()) const override
+    {
+        return 200;
+    }
+
+    QVariant data(const QModelIndex &index, int role) const override
+    {
+        switch (role) {
+            case Qt::DisplayRole:
+                return QString("%1, %2").arg(index.column()).arg(index.row());
+            default:
+                break;
+        }
+
+        return QVariant();
+    }
+
+    QHash<int, QByteArray> roleNames() const override
+    {
+        return { {Qt::DisplayRole, "display"} };
+    }
+};
