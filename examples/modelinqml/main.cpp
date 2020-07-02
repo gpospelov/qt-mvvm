f50b8155398360e6c1b824226e6780895c99acc3
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-07-02 12:20:05 +0200

    Correct registration of TableModel for QML

--- examples/modelinqml/main.cpp
+++ examples/modelinqml/main.cpp
@@ -1,10 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <modelinqmlcore/tablemodel.h>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    qmlRegisterType<TableModel>("TableModel", 0, 1, "TableModel");
+
     QQmlApplicationEngine engine("qrc:/main.qml");
+
     engine.addImportPath("./plugins");
     return app.exec();
 }
