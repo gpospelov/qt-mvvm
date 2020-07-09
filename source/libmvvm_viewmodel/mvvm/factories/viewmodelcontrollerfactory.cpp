4dff67684853afbe74a9f8cec53e17a4c8a1576f
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-07-09 15:23:16 +0200

    Basic implementation of ViewModelControllerBuilder

--- source/libmvvm_viewmodel/mvvm/factories/viewmodelcontrollerfactory.cpp
+++ source/libmvvm_viewmodel/mvvm/factories/viewmodelcontrollerfactory.cpp
@@ -8,7 +8,59 @@
 // ************************************************************************** //
 
 #include <mvvm/factories/viewmodelcontrollerfactory.h>
+#include <mvvm/interfaces/childrenstrategyinterface.h>
+#include <mvvm/interfaces/rowstrategyinterface.h>
 #include <mvvm/viewmodel/standardviewmodelcontrollers.h>
+#include <stdexcept>
 
+namespace ModelView
+{
 
+ViewModelControllerBuilder::ViewModelControllerBuilder() = default;
 
+ViewModelControllerBuilder::~ViewModelControllerBuilder() = default;
+
+ViewModelControllerBuilder::operator std::unique_ptr<ViewModelController>()
+{
+    if (!context.model || !context.view_model)
+        throw std::runtime_error("Error in ViewModelController: undefined models");
+
+    if (!context.children_strategy)
+        throw std::runtime_error("Error in ViewModelController: no children strategy defined.");
+
+    if (!context.row_strategy)
+        throw std::runtime_error("Error in ViewModelController: no row strategy defined.");
+
+    auto result = std::make_unique<ViewModelController>(context.model, context.view_model);
+    result->setChildrenStrategy(std::move(context.children_strategy));
+    result->setRowStrategy(std::move(context.row_strategy));
+    return result;
+}
+
+ViewModelControllerBuilder::self& ViewModelControllerBuilder::model(SessionModel* model)
+{
+    context.model = model;
+    return *this;
+}
+
+ViewModelControllerBuilder::self& ViewModelControllerBuilder::viewModel(ViewModelBase* view_model)
+{
+    context.view_model = view_model;
+    return *this;
+}
+
+ViewModelControllerBuilder::self& ViewModelControllerBuilder::childrenStrategy(
+    std::unique_ptr<ChildrenStrategyInterface> children_strategy)
+{
+    context.children_strategy = std::move(children_strategy);
+    return *this;
+}
+
+ViewModelControllerBuilder::self&
+ViewModelControllerBuilder::rowStrategy(std::unique_ptr<RowStrategyInterface> row_strategy)
+{
+    context.row_strategy = std::move(row_strategy);
+    return *this;
+}
+
+} // namespace ModelView
--- source/libmvvm_viewmodel/mvvm/factories/viewmodelcontrollerfactory.h
+++ source/libmvvm_viewmodel/mvvm/factories/viewmodelcontrollerfactory.h
@@ -10,16 +10,46 @@
 #ifndef MVVM_FACTORIES_VIEWMODELCONTROLLERFACTORY_H
 #define MVVM_FACTORIES_VIEWMODELCONTROLLERFACTORY_H
 
-#include <mvvm/viewmodel_export.h>
 #include <memory>
+#include <mvvm/viewmodel_export.h>
 
 namespace ModelView
 {
 
-namespace Factory
-{
+class SessionModel;
+class ViewModelBase;
+class ChildrenStrategyInterface;
+class RowStrategyInterface;
+class ViewModelController;
 
-}
+class ViewModelControllerBuilder
+{
+public:
+    using self = ViewModelControllerBuilder;
+
+    ViewModelControllerBuilder();
+    ~ViewModelControllerBuilder();
+
+    ViewModelControllerBuilder(const ViewModelControllerBuilder& other) = delete;
+    ViewModelControllerBuilder& operator=(const ViewModelControllerBuilder& other) = delete;
+
+    self& model(SessionModel* model);
+    self& viewModel(ViewModelBase* view_model);
+    self& childrenStrategy(std::unique_ptr<ChildrenStrategyInterface> children_strategy);
+    self& rowStrategy(std::unique_ptr<RowStrategyInterface> row_strategy);
+
+    operator std::unique_ptr<ViewModelController>();
+
+private:
+    struct Context {
+        SessionModel* model{nullptr};
+        ViewModelBase* view_model{nullptr};
+        std::unique_ptr<ChildrenStrategyInterface> children_strategy;
+        std::unique_ptr<RowStrategyInterface> row_strategy;
+    };
+
+    Context context;
+};
 
 } // namespace ModelView
 
