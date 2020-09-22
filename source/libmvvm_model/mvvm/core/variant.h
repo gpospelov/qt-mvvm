9bc60e67faf80ad065d6fcebcdd9c26d0dcceca1
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-09-22 09:55:13 +0200

    Introduce variant.h as a first step toward migration to std::variant

---
+++ source/libmvvm_model/mvvm/core/variant.h
@@ -0,0 +1,18 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#ifndef MVVM_MODEL_VARIANT_H
+#define MVVM_MODEL_VARIANT_H
+
+//! @file variant.h
+//! Defines variant type (intermediate step toward QVariant ==> std::variant migration).
+
+#include <QVariant>
+
+#endif // MVVM_MODEL_UNIQUEIDGENERATOR_H
