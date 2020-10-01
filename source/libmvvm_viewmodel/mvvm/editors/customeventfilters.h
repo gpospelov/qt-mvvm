0d515af6b5c2b1ae5f008519e2f57022bc171f8f
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-10-01 09:00:12 +0200

    Add new event filter to ignore wheel events

--- source/libmvvm_viewmodel/mvvm/editors/customeventfilters.h
+++ source/libmvvm_viewmodel/mvvm/editors/customeventfilters.h
@@ -16,7 +16,10 @@
 namespace ModelView
 {
 
-//! Event filter to prevent lost of focus by custom material editor.
+//! Event filter to prevent loss of the focus.
+//! Can be used in the context of QTreeView and similar widgets to call external editor. Such an
+//! editor is created by clicking on a cell of a tree and it appears as  modal window on top of a
+//! tree.
 
 class MVVM_VIEWMODEL_EXPORT LostFocusFilter : public QObject
 {
@@ -26,6 +29,19 @@ public:
 
 protected:
     bool eventFilter(QObject* obj, QEvent* event) override;
+};
+
+//! Event filter to install on combo boxes and spin boxes to ignore wheel events during scrolling.
+//! Helpful than the spin box is a child of some larger scroll area.
+
+class MVVM_VIEWMODEL_EXPORT WheelEventFilter : public QObject
+{
+    Q_OBJECT
+public:
+    WheelEventFilter(QObject* parent = nullptr);
+
+protected:
+    bool eventFilter(QObject* obj, QEvent* event);
 };
 
 } // namespace ModelView
