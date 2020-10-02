58d60d9fca3c791d90f53b5573ba7b0cbda5976a
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-10-02 16:15:15 +0200

    Introduce AbstractEditorFactory

--- source/libmvvm_viewmodel/mvvm/editors/defaulteditorfactory.cpp
+++ source/libmvvm_viewmodel/mvvm/editors/defaulteditorfactory.cpp
@@ -30,18 +30,21 @@ const SessionItem* itemFromIndex(const QModelIndex& index)
 
 // ----------------------------------------------------------------------------
 
-std::unique_ptr<CustomEditor> EditorFactory::createEditor(const QModelIndex& index) const
+//! Creates editor for given model index. It is expected that the index belongs to a ViewModel.
+
+std::unique_ptr<CustomEditor> AbstractEditorFactory::createEditor(const QModelIndex& index) const
 {
     auto item = itemFromIndex(index);
-    return item ? createEditor(*item) : std::unique_ptr<CustomEditor>();
+    return item ? createItemEditor(*item) : std::unique_ptr<CustomEditor>();
 }
 
-void EditorFactory::registerBuilder(const std::string& name, EditorBuilders::builder_t builder)
+void AbstractEditorFactory::registerBuilder(const std::string& name,
+                                            EditorBuilders::builder_t builder)
 {
     m_nameToBuilderMap[name] = std::move(builder);
 }
 
-EditorBuilders::builder_t EditorFactory::findBuilder(const std::string& name) const
+EditorBuilders::builder_t AbstractEditorFactory::findBuilder(const std::string& name) const
 {
     auto it = m_nameToBuilderMap.find(name);
     return it != m_nameToBuilderMap.end() ? it->second : EditorBuilders::builder_t();
@@ -49,32 +52,9 @@ EditorBuilders::builder_t EditorFactory::findBuilder(const std::string& name) co
 
 // ----------------------------------------------------------------------------
 
-VariantDependentEditorFactory::VariantDependentEditorFactory()
-{
-    registerBuilder(Constants::bool_type_name, EditorBuilders::BoolEditorBuilder());
-    registerBuilder(Constants::int_type_name, EditorBuilders::IntegerEditorBuilder());
-    registerBuilder(Constants::double_type_name, EditorBuilders::ScientificSpinBoxEditorBuilder());
-    registerBuilder(Constants::qcolor_type_name, EditorBuilders::ColorEditorBuilder());
-    registerBuilder(Constants::comboproperty_type_name,
-                    EditorBuilders::ComboPropertyEditorBuilder());
-    registerBuilder(Constants::extproperty_type_name,
-                    EditorBuilders::ExternalPropertyEditorBuilder());
-}
-
-//! Creates cell editor basing on variant name.
-
-std::unique_ptr<CustomEditor>
-VariantDependentEditorFactory::createEditor(const SessionItem& item) const
-{
-    auto value = item.data<QVariant>();
-    auto builder = findBuilder(Utils::VariantName(value));
-    return builder ? builder(&item) : std::unique_ptr<CustomEditor>();
-}
-
-// ----------------------------------------------------------------------------
-
 RoleDependentEditorFactory::RoleDependentEditorFactory()
 {
+    // registering set of builders for given editor types
     registerBuilder(Constants::BoolEditorType, EditorBuilders::BoolEditorBuilder());
     registerBuilder(Constants::ColorEditorType, EditorBuilders::ColorEditorBuilder());
     registerBuilder(Constants::ComboPropertyEditorType,
@@ -91,10 +71,10 @@ RoleDependentEditorFactory::RoleDependentEditorFactory()
                     EditorBuilders::SelectableComboPropertyEditorBuilder());
 }
 
-//! Creates cell editor basing on variant name.
+//! Creates cell editor basing on editor type.
 
 std::unique_ptr<CustomEditor>
-RoleDependentEditorFactory::createEditor(const SessionItem& item) const
+RoleDependentEditorFactory::createItemEditor(const SessionItem& item) const
 {
     auto builder = findBuilder(item.editorType());
     return builder ? builder(&item) : std::unique_ptr<CustomEditor>();
@@ -102,88 +82,43 @@ RoleDependentEditorFactory::createEditor(const SessionItem& item) const
 
 // ----------------------------------------------------------------------------
 
-DefaultEditorFactory::~DefaultEditorFactory() = default;
-
-DefaultEditorFactory::DefaultEditorFactory()
+VariantDependentEditorFactory::VariantDependentEditorFactory()
 {
-    initBuildersForVariantName();
-    initBuildersForEditorType();
+    // registering set of builders for given variant names
+    registerBuilder(Constants::bool_type_name, EditorBuilders::BoolEditorBuilder());
+    registerBuilder(Constants::int_type_name, EditorBuilders::IntegerEditorBuilder());
+    registerBuilder(Constants::double_type_name, EditorBuilders::ScientificSpinBoxEditorBuilder());
+    registerBuilder(Constants::qcolor_type_name, EditorBuilders::ColorEditorBuilder());
+    registerBuilder(Constants::comboproperty_type_name,
+                    EditorBuilders::ComboPropertyEditorBuilder());
+    registerBuilder(Constants::extproperty_type_name,
+                    EditorBuilders::ExternalPropertyEditorBuilder());
 }
 
-//! Creates editor for given model index. It is expected that the index belongs to a ViewModel.
+//! Creates cell editor basing on variant name.
+
+std::unique_ptr<CustomEditor>
+VariantDependentEditorFactory::createItemEditor(const SessionItem& item) const
+{
+    auto value = item.data<QVariant>();
+    auto builder = findBuilder(Utils::VariantName(value));
+    return builder ? builder(&item) : std::unique_ptr<CustomEditor>();
+}
+
+// ----------------------------------------------------------------------------
+
+DefaultEditorFactory::DefaultEditorFactory()
+    : m_roleDependentFactory(std::make_unique<RoleDependentEditorFactory>()),
+      m_variantDependentFactory(std::make_unique<VariantDependentEditorFactory>())
+{
+}
+
+//! Creates editor for given model index.
 
 std::unique_ptr<CustomEditor> DefaultEditorFactory::createEditor(const QModelIndex& index) const
 {
-    auto item = itemFromIndex(index);
-    if (!item)
-        return {};
-
-    auto value = item->data<QVariant>();
-    auto builder = findBuilderForVariant(Utils::VariantName(value));
-    return builder ? builder(item) : std::unique_ptr<CustomEditor>();
-}
-
-//! Creates map of builders to retrieve the builder from variant name.
-
-void DefaultEditorFactory::initBuildersForVariantName()
-{
-    registerBuilderForVariant(Constants::bool_type_name, EditorBuilders::BoolEditorBuilder());
-    registerBuilderForVariant(Constants::int_type_name, EditorBuilders::IntegerEditorBuilder());
-    registerBuilderForVariant(Constants::double_type_name,
-                              EditorBuilders::ScientificSpinBoxEditorBuilder());
-    registerBuilderForVariant(Constants::qcolor_type_name, EditorBuilders::ColorEditorBuilder());
-    registerBuilderForVariant(Constants::comboproperty_type_name,
-                              EditorBuilders::ComboPropertyEditorBuilder());
-    registerBuilderForVariant(Constants::extproperty_type_name,
-                              EditorBuilders::ExternalPropertyEditorBuilder());
-}
-
-//! Creates map of builders to retrieve the builder from editor type.
-
-void DefaultEditorFactory::initBuildersForEditorType()
-{
-    registerBuilderForEditor(Constants::BoolEditorType, EditorBuilders::BoolEditorBuilder());
-    registerBuilderForEditor(Constants::ColorEditorType, EditorBuilders::ColorEditorBuilder());
-    registerBuilderForEditor(Constants::ComboPropertyEditorType,
-                             EditorBuilders::ComboPropertyEditorBuilder());
-    registerBuilderForEditor(Constants::DoubleEditorType, EditorBuilders::DoubleEditorBuilder());
-    registerBuilderForEditor(Constants::ExternalPropertyEditorType,
-                             EditorBuilders::ExternalPropertyEditorBuilder());
-    registerBuilderForEditor(Constants::IntegerEditorType, EditorBuilders::IntegerEditorBuilder());
-    registerBuilderForEditor(Constants::ScientficDoubleEditorType,
-                             EditorBuilders::ScientificDoubleEditorBuilder());
-    registerBuilderForEditor(Constants::ScientficSpinBoxEditorType,
-                             EditorBuilders::ScientificSpinBoxEditorBuilder());
-    registerBuilderForEditor(Constants::SelectableComboPropertyEditorType,
-                             EditorBuilders::SelectableComboPropertyEditorBuilder());
-}
-
-void DefaultEditorFactory::registerBuilderForVariant(const std::string& variant_type,
-                                                     EditorBuilders::builder_t builder)
-{
-    m_variantNameToBuilderMap[variant_type] = std::move(builder);
-}
-
-void DefaultEditorFactory::registerBuilderForEditor(const std::string& editor_type,
-                                                    EditorBuilders::builder_t builder)
-{
-    m_variantNameToBuilderMap[editor_type] = std::move(builder);
-}
-
-//! Returns builder for variant with given name.
-
-EditorBuilders::builder_t
-DefaultEditorFactory::findBuilderForVariant(const std::string& variant_name) const
-{
-    auto it = m_variantNameToBuilderMap.find(variant_name);
-    return it != m_variantNameToBuilderMap.end() ? it->second : EditorBuilders::builder_t();
-}
-
-//! Returns builder for editor with given name.
-
-EditorBuilders::builder_t
-DefaultEditorFactory::findBuilderForEditor(const std::string& editor_type) const
-{
-    auto it = m_editorTypeToBuilderMap.find(editor_type);
-    return it != m_editorTypeToBuilderMap.end() ? it->second : EditorBuilders::builder_t();
+    // trying to created an editor basing on possibly defined EDITOR role
+    auto editor = m_roleDependentFactory->createEditor(index);
+    // if we do not succeed, then creating editor from variant type
+    return editor ? std::move(editor) : m_variantDependentFactory->createEditor(index);
 }
--- source/libmvvm_viewmodel/mvvm/editors/defaulteditorfactory.h
+++ source/libmvvm_viewmodel/mvvm/editors/defaulteditorfactory.h
@@ -11,22 +11,24 @@
 #define MVVM_EDITORS_DEFAULTEDITORFACTORY_H
 
 #include <map>
+#include <memory>
 #include <mvvm/editors/editorbuilders.h>
 #include <mvvm/interfaces/editorfactoryinterface.h>
 
 namespace ModelView
 {
 
-//! Base class for all editor factories.
-//! Used in the context of trees and tables via ViewModelDelegate.
+//! Abstract editor factory for ViewModelDelegate.
+//! Creates cell editors for Qt trees and tables basing on model index. Cell editor is
+//! Qt widget intended for editing DATA role of some SessionItem.
 
-class MVVM_VIEWMODEL_EXPORT EditorFactory : public EditorFactoryInterface
+class MVVM_VIEWMODEL_EXPORT AbstractEditorFactory : public EditorFactoryInterface
 {
 public:
     std::unique_ptr<CustomEditor> createEditor(const QModelIndex& index) const override;
 
 protected:
-    virtual std::unique_ptr<CustomEditor> createEditor(const SessionItem& item) const = 0;
+    virtual std::unique_ptr<CustomEditor> createItemEditor(const SessionItem& item) const = 0;
 
     void registerBuilder(const std::string& name, EditorBuilders::builder_t builder);
     EditorBuilders::builder_t findBuilder(const std::string& name) const;
@@ -34,52 +36,43 @@ protected:
     std::map<std::string, EditorBuilders::builder_t> m_nameToBuilderMap;
 };
 
-//! Creates cell editors for trees and tables basing on variant name.
+//! Editor factory for cell editors in Qt trees and tables, relies on EDITORTYPE role stored
+//! on board of SessionItem.
 
-class MVVM_VIEWMODEL_EXPORT VariantDependentEditorFactory : public EditorFactory
-{
-public:
-    VariantDependentEditorFactory();
-
-protected:
-    std::unique_ptr<CustomEditor> createEditor(const SessionItem& item) const override;
-};
-
-//! Creates cell editors for trees and tables basing on possible EDITORTYPE role encoded in item.
-
-class MVVM_VIEWMODEL_EXPORT RoleDependentEditorFactory : public EditorFactory
+class MVVM_VIEWMODEL_EXPORT RoleDependentEditorFactory : public AbstractEditorFactory
 {
 public:
     RoleDependentEditorFactory();
 
 protected:
-    std::unique_ptr<CustomEditor> createEditor(const SessionItem& item) const override;
+    std::unique_ptr<CustomEditor> createItemEditor(const SessionItem& item) const override;
 };
 
-//! Default editor factory for custom variants.
-//! Used in the context of trees and tables via ViewModelDelegate.
+//! Editor factory for cell editors in Qt trees and tables, relies on variant type stored as
+//! DATA role on board of SessionItem.
+
+class MVVM_VIEWMODEL_EXPORT VariantDependentEditorFactory : public AbstractEditorFactory
+{
+public:
+    VariantDependentEditorFactory();
+
+protected:
+    std::unique_ptr<CustomEditor> createItemEditor(const SessionItem& item) const override;
+};
+
+//! Default editor factory for cell editors in Qt trees and tables.
+//! Internaly it uses two factories
 
 class MVVM_VIEWMODEL_EXPORT DefaultEditorFactory : public EditorFactoryInterface
 {
 public:
     DefaultEditorFactory();
-    ~DefaultEditorFactory() override;
 
     std::unique_ptr<CustomEditor> createEditor(const QModelIndex& index) const override;
 
-protected:
-    void initBuildersForVariantName();
-    void initBuildersForEditorType();
-    void registerBuilderForVariant(const std::string& variant_name,
-                                   EditorBuilders::builder_t builder);
-    void registerBuilderForEditor(const std::string& editor_type,
-                                  EditorBuilders::builder_t builder);
-
-    EditorBuilders::builder_t findBuilderForVariant(const std::string& variant_name) const;
-    EditorBuilders::builder_t findBuilderForEditor(const std::string& editor_type) const;
-
-    std::map<std::string, EditorBuilders::builder_t> m_variantNameToBuilderMap;
-    std::map<std::string, EditorBuilders::builder_t> m_editorTypeToBuilderMap;
+private:
+    std::unique_ptr<RoleDependentEditorFactory> m_roleDependentFactory;
+    std::unique_ptr<VariantDependentEditorFactory> m_variantDependentFactory;
 };
 
 } // namespace ModelView
