f9aac42078c1e590ed3f825184574247910aa99e
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-10-29 22:31:19 +0100

    Convenience function to convert parameter pack to vector

--- tests/libtestmachinery/test_utils.h
+++ tests/libtestmachinery/test_utils.h
@@ -94,14 +94,21 @@ template <typename T> auto create_pointers(const std::vector<std::unique_ptr<T>>
 
 //! Creates vector of strings from argument list.
 
-template <typename T = std::string, typename... Args>
-std::vector<T> toStringVector(Args&&... args)
+template <typename T = std::string, typename... Args> std::vector<T> toStringVector(Args&&... args)
 {
     std::vector<T> v;
     (v.push_back(std::string(args)), ...);
     return v;
 }
 
+//! Creates vector of T from argument list. Used in EXPECT_EQ macros for convenience.
+
+template <typename T = std::string, typename... Args> std::vector<T> toVector(Args&&... args)
+{
+    std::vector<T> v;
+    (v.push_back(T(args)), ...);
+    return v;
+}
 
 } // namespace TestUtils
 
