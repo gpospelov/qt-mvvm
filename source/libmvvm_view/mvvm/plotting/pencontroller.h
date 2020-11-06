4978873c62cbdae686e9fbe3eb8c3f3929eb7e24
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-11-06 18:16:47 +0100

    Fix for windows build

--- source/libmvvm_view/mvvm/plotting/pencontroller.h
+++ source/libmvvm_view/mvvm/plotting/pencontroller.h
@@ -13,6 +13,7 @@
 #include <memory>
 #include <mvvm/signals/itemlistener.h>
 #include <mvvm/view_export.h>
+#include <mvvm/standarditems/plottableitems.h>
 
 class QCPGraph;
 
@@ -29,6 +30,9 @@ class MVVM_VIEW_EXPORT PenController : public ItemListener<PenItem>
 public:
     explicit PenController(QCPGraph* graph);
     ~PenController() override;
+
+    PenController& operator=(const PenController& other) = delete;
+    PenController(const PenController& other) = delete;
 
 protected:
     void subscribe() override;
