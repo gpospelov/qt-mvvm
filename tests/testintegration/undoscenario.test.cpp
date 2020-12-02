f76ec45b10ac8d2d55332927db0db4c4568d98d6
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-12-02 18:11:01 +0100

    Implement failing test which reveals the problem of undo during axis range change

--- tests/testintegration/undoscenario.test.cpp
+++ tests/testintegration/undoscenario.test.cpp
@@ -8,6 +8,13 @@
 // ************************************************************************** //
 
 #include "google_test.h"
+#include "qcustomplot.h"
+#include <mvvm/interfaces/undostackinterface.h>
+#include <mvvm/model/sessionmodel.h>
+#include <mvvm/plotting/viewportaxisplotcontroller.h>
+#include <mvvm/standarditems/axisitems.h>
+
+using namespace ModelView;
 
 //! Testing various undo/redo scenario.
 
@@ -19,7 +26,57 @@ public:
 
 UndoScenarioTest::~UndoScenarioTest() = default;
 
-TEST_F(UndoScenarioTest, initialState)
+//! Check undo/redo of ViewportAxisItem range, when it is listened by controller.
+//! Real-life bug.
+
+TEST_F(UndoScenarioTest, undoViewportSetRange)
 {
-    EXPECT_EQ(1, 1);
+    // initialzing model, custom plot and controller
+    SessionModel model;
+    auto axisItem = model.insertItem<ViewportAxisItem>();
+    axisItem->setProperty(ViewportAxisItem::P_MIN, 1.0);
+    axisItem->setProperty(ViewportAxisItem::P_MAX, 2.0);
+    QCustomPlot custom_plot;
+    ViewportAxisPlotController controller(custom_plot.xAxis);
+    controller.setItem(axisItem);
+
+    // initial axis state
+    EXPECT_EQ(custom_plot.xAxis->range().lower, 1.0);
+    EXPECT_EQ(custom_plot.xAxis->range().upper, 2.0);
+
+    // enabling undo/redo, and  its initial state
+    model.setUndoRedoEnabled(true);
+    auto stack = model.undoStack();
+    EXPECT_FALSE(stack->canRedo());
+    EXPECT_FALSE(stack->canUndo());
+    EXPECT_EQ(stack->index(), 0);
+    EXPECT_EQ(stack->count(), 0);
+
+    // changing axis
+    axisItem->setProperty(ViewportAxisItem::P_MAX, 20.0);
+    EXPECT_FALSE(stack->canRedo());
+    EXPECT_TRUE(stack->canUndo());
+    EXPECT_EQ(stack->index(), 1);
+    EXPECT_EQ(stack->count(), 1);
+    EXPECT_EQ(custom_plot.xAxis->range().lower, 1.0);
+    EXPECT_EQ(custom_plot.xAxis->range().upper, 20.0);
+
+    // undoing
+    stack->undo();
+    EXPECT_TRUE(stack->canRedo());
+    EXPECT_FALSE(stack->canUndo());
+    EXPECT_EQ(stack->index(), 0);
+    EXPECT_EQ(stack->count(), 1);
+    EXPECT_EQ(custom_plot.xAxis->range().lower, 1.0);
+    EXPECT_EQ(custom_plot.xAxis->range().upper, 2.0);
+
+    // redoing
+    // FIXME uncomment tests below, there is segfault
+    //    stack->redo();
+    //    EXPECT_FALSE(stack->canRedo());
+    //    EXPECT_TRUE(stack->canUndo());
+    //    EXPECT_EQ(stack->index(), 1);
+    //    EXPECT_EQ(stack->count(), 1);
+    //    EXPECT_EQ(custom_plot.xAxis->range().lower, 1.0);
+    //    EXPECT_EQ(custom_plot.xAxis->range().upper, 20.0);
 }
