3e6afb3989a4353a98ed38c1b077d77477688616
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-12-04 16:32:56 +0100

    Create a file with list of all standard item includes

---
+++ source/libmvvm_model/mvvm/standarditems/standarditemincludes.h
@@ -0,0 +1,30 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#ifndef MVVM_STANDARDITEMS_STANDARDITEMINCLUDES_H
+#define MVVM_STANDARDITEMS_STANDARDITEMINCLUDES_H
+
+//! @file standarditemincludes.h
+//! Collection of all includes with items supported by the model out-of-the-box.
+
+#include <mvvm/model/compounditem.h>
+#include <mvvm/model/propertyitem.h>
+#include <mvvm/standarditems/axisitems.h>
+#include <mvvm/standarditems/colormapitem.h>
+#include <mvvm/standarditems/colormapviewportitem.h>
+#include <mvvm/standarditems/containeritem.h>
+#include <mvvm/standarditems/data1ditem.h>
+#include <mvvm/standarditems/data2ditem.h>
+#include <mvvm/standarditems/graphitem.h>
+#include <mvvm/standarditems/graphviewportitem.h>
+#include <mvvm/standarditems/linkeditem.h>
+#include <mvvm/standarditems/plottableitems.h>
+#include <mvvm/standarditems/vectoritem.h>
+
+#endif // MVVM_STANDARDITEMS_STANDARDITEMINCLUDES_H
