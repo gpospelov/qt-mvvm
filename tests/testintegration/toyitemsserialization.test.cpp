119f8121363dc8879884c63c046a35b7c88018cd
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-12-04 16:11:06 +0100

    Extend unit tests for the serialization of toy items

--- tests/testintegration/toyitemsserialization.test.cpp
+++ tests/testintegration/toyitemsserialization.test.cpp
@@ -14,6 +14,7 @@
 #include <mvvm/serialization/jsonmodelconverter.h>
 
 using namespace ModelView;
+using namespace ToyItems;
 
 //! Testing serialization of ToyItems using json converters.
 
@@ -21,9 +22,9 @@ class ToyItemsSerializationTest : public ::testing::Test
 {
 public:
     //! Returns copy of model obtrained via json serialization.
-    std::unique_ptr<ToyItems::SampleModel> createCopy(const ToyItems::SampleModel& model)
+    std::unique_ptr<SampleModel> createCopy(const SampleModel& model)
     {
-        auto result = std::make_unique<ToyItems::SampleModel>();
+        auto result = std::make_unique<SampleModel>();
         JsonModelConverter converter;
         QJsonObject object = converter.to_json(model);
         converter.from_json(object, *result);
@@ -40,12 +41,59 @@ ToyItemsSerializationTest::~ToyItemsSerializationTest() = default;
 
 TEST_F(ToyItemsSerializationTest, defaultShapeGroupItemInModel)
 {
-    ToyItems::SampleModel model;
-    auto group = model.insertItem<ToyItems::ShapeGroupItem>();
+    // model with single group item
+    SampleModel model;
+    auto group = model.insertItem<ShapeGroupItem>();
 
+    // copy of model
     auto modelCopy = createCopy(model);
-    auto groupCopy = modelCopy->topItem<ToyItems::ShapeGroupItem>();
+    auto groupCopy = modelCopy->topItem<ShapeGroupItem>();
 
+    // basic properties in copied item should be the same
     EXPECT_EQ(group->currentIndex(), groupCopy->currentIndex());
     EXPECT_EQ(group->currentItem()->modelType(), groupCopy->currentItem()->modelType());
+}
+
+//! Checking ShapeGroupItem in a model.
+//! Serialization/deserelization should give an item identical to original.
+
+TEST_F(ToyItemsSerializationTest, modifiedShapeGroupItemInModel)
+{
+    SampleModel model;
+    auto group = model.insertItem<ShapeGroupItem>();
+
+    // modifying group item
+    group->setCurrentType(ToyItems::Constants::AnysoPyramidItemType);
+    group->children().at(0)->setProperty(CylinderItem::P_RADIUS, 42.0);
+    group->children().at(1)->setProperty(SphereItem::P_RADIUS, 43.0);
+    group->children().at(2)->setProperty(AnysoPyramidItem::P_LENGTH, 44.0);
+
+    // creating copy
+    auto modelCopy = createCopy(model);
+    auto groupCopy = modelCopy->topItem<ShapeGroupItem>();
+
+    // checking properties of
+    EXPECT_EQ(groupCopy->currentIndex(), group->currentIndex());
+    EXPECT_EQ(groupCopy->currentItem()->modelType(), group->currentItem()->modelType());
+    EXPECT_EQ(groupCopy->children().at(0)->property<double>(CylinderItem::P_RADIUS), 42.0);
+    EXPECT_EQ(groupCopy->children().at(1)->property<double>(SphereItem::P_RADIUS), 43.0);
+    EXPECT_EQ(groupCopy->children().at(2)->property<double>(AnysoPyramidItem::P_LENGTH), 44.0);
+}
+
+//! Insert all supported items in a model and check that after serialization
+
+TEST_F(ToyItemsSerializationTest, allItemsInAModel)
+{
+    SampleModel model;
+    model.insertItem<ToyItems::MultiLayerItem>();
+    model.insertItem<ToyItems::LayerItem>();
+    model.insertItem<ToyItems::ParticleItem>();
+    model.insertItem<ToyItems::InterferenceFunctionItem>();
+    model.insertItem<ToyItems::SphereItem>();
+    model.insertItem<ToyItems::CylinderItem>();
+    model.insertItem<ToyItems::AnysoPyramidItem>();
+    model.insertItem<ToyItems::ShapeGroupItem>();
+
+    auto modelCopy = createCopy(model);
+    EXPECT_EQ(model.rootItem()->childrenCount(), modelCopy->rootItem()->childrenCount());
 }
