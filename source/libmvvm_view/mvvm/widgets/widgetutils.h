0eaf98b019e3daa310edff6902a5cba7a1c65c2e
Gennady Pospelov <gennady.pospelov@gmail.com>
2020-12-22 16:40:03 +0100

    Modernize namespace declaration

--- source/libmvvm_view/mvvm/widgets/widgetutils.h
+++ source/libmvvm_view/mvvm/widgets/widgetutils.h
@@ -21,11 +21,9 @@ class QSize;
 class QMainWindow;
 class QLabel;
 
-namespace ModelView {
-
 //! Collection of various widget-related utils.
 
-namespace Utils {
+namespace ModelView::Utils {
 
 //! Returns random color.
 MVVM_VIEW_EXPORT QColor RandomColor();
@@ -82,8 +80,6 @@ MVVM_VIEW_EXPORT QByteArray serialize(const QStringList& data);
 //! Converts byte array to vector of strings.
 MVVM_VIEW_EXPORT QStringList deserialize(const QByteArray& byteArray);
 
-} // namespace Utils
-
-} // namespace ModelView
+} // namespace ModelView::Utils
 
 #endif // MVVM_WIDGETS_UTILS_H
