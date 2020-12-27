1c0bd81c9f466e981f864f0b011ad83f78ce33c8
Gennady Pospelov <gennady.pospelov@gmail.com>
2020-12-27 17:46:30 +0100

    Fix warning while referring oerriden method

--- examples/graphicsproxy/graphicsproxycore/graphicsview.h
+++ examples/graphicsproxy/graphicsproxycore/graphicsview.h
@@ -23,7 +23,7 @@ public:
     GraphicsView(GraphicsScene* scene, QWidget* parent);
 
 protected:
-    void resizeEvent(QResizeEvent* event);
+    void resizeEvent(QResizeEvent* event) override;
 
 private:
     GraphicsScene* scene{nullptr};
