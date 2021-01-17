67cf03c2fa2b1a11aaa5819a655759f0f11f83e7
Gennady Pospelov <gennady.pospelov@gmail.com>
2021-01-17 10:44:48 +0100

    Extract ConnectableItem to a separate file

--- examples/nodeeditor/nodeeditorcore/sampleitems.cpp
+++ examples/nodeeditor/nodeeditorcore/sampleitems.cpp
@@ -8,64 +8,13 @@
 // ************************************************************************** //
 
 #include "sampleitems.h"
-#include "mvvm/editors/editor_constants.h"
-#include "mvvm/model/taginfo.h"
-#include "mvvm/widgets/widgetutils.h"
 #include <QColor>
 
 using namespace ModelView;
 
 namespace NodeEditor {
 
-ConnectableItem::ConnectableItem(const std::string& modelType) : ModelView::CompoundItem(modelType)
-{
-    addProperty(P_XPOS, 0.0)->setDisplayName("X");
-    addProperty(P_YPOS, 0.0)->setDisplayName("Y");
-    addProperty(P_COLOR, QColor(Qt::gray))->setDisplayName("Color");
-}
-
-QColor ConnectableItem::color() const
-{
-    return property<QColor>(P_COLOR);
-}
-
-double ConnectableItem::x() const
-{
-    return property<double>(P_XPOS);
-}
-
-void ConnectableItem::setX(double x)
-{
-    setProperty(P_XPOS, x);
-}
-
-double ConnectableItem::y() const
-{
-    return property<double>(P_YPOS);
-}
-
-void ConnectableItem::setY(double y)
-{
-    setProperty(P_YPOS, y);
-}
-
-//! Sets named color following schema from https://www.w3.org/TR/css-color-3/#svg-color.
-//! e.g. "mediumaquamarine"
-
-void ConnectableItem::setNamedColor(const std::string& named_color)
-{
-    setProperty(P_COLOR, QColor(QString::fromStdString(named_color)));
-}
-
-std::vector<PortInfo> ConnectableItem::inputPorts() const
-{
-    return {};
-}
-
-std::vector<PortInfo> ConnectableItem::outputPorts() const
-{
-    return {};
-}
+// ----------------------------------------------------------------------------
 
 ParticleItem::ParticleItem() : ConnectableItem(ParticleItemType)
 {
@@ -78,6 +27,8 @@ std::vector<PortInfo> ParticleItem::inputPorts() const
 {
     return {TransformationPort};
 }
+
+// ----------------------------------------------------------------------------
 
 TransformationItem::TransformationItem() : ConnectableItem(TransformationItemType)
 {
--- examples/nodeeditor/nodeeditorcore/sampleitems.h
+++ examples/nodeeditor/nodeeditorcore/sampleitems.h
@@ -13,40 +13,12 @@
 //! @file sampleitems.h
 //! Classes to represent content of node editor.
 
-#include "mvvm/model/compounditem.h"
-#include "portinfo.h"
-
-class QColor;
+#include "connectableitem.h"
 
 namespace NodeEditor {
 
 const std::string ParticleItemType = "Particle";
 const std::string TransformationItemType = "Transformation";
-
-//! Base class for all node editor items.
-
-class ConnectableItem : public ModelView::CompoundItem {
-public:
-    static inline const std::string P_XPOS = "P_XPOS";
-    static inline const std::string P_YPOS = "P_YPOS";
-    static inline const std::string P_COLOR = "P_COLOR";
-
-    explicit ConnectableItem(const std::string& modelType);
-
-    QColor color() const;
-
-    double x() const;
-    void setX(double x);
-
-    double y() const;
-    void setY(double y);
-
-    void setNamedColor(const std::string& named_color);
-
-    virtual std::vector<PortInfo> inputPorts() const;
-
-    virtual std::vector<PortInfo> outputPorts() const;
-};
 
 //! Represents particle item box on graph canvas.
 
