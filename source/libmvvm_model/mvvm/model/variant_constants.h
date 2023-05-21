7350bb60d0614811d289d4daed6c360ae5f7ad31
Gennady Pospelov <gennady.pospelov@iter.org>
2023-05-21 22:09:40 +0200

    Fix for variant name in Qt

--- source/libmvvm_model/mvvm/model/variant_constants.h
+++ source/libmvvm_model/mvvm/model/variant_constants.h
@@ -11,6 +11,7 @@
 #define MVVM_MODEL_VARIANT_CONSTANTS_H
 
 #include <string>
+#include <QtGlobal>
 
 //! @file variant-constants.h
 //! Collection of constants with supported variant names.
@@ -20,13 +21,21 @@ namespace ModelView::Constants {
 const std::string invalid_type_name = "invalid";
 const std::string bool_type_name = "bool";
 const std::string int_type_name = "int";
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 const std::string string_type_name = "std::string";
+#else
+//! FIXME find less fragile way of deducying type of the variant
+const std::string string_type_name = "std::__cxx11::basic_string<char>";
+#endif
+
 const std::string double_type_name = "double";
 const std::string vector_double_type_name = "std::vector<double>";
 const std::string comboproperty_type_name = "ModelView::ComboProperty";
 const std::string qcolor_type_name = "QColor";
 const std::string extproperty_type_name = "ModelView::ExternalProperty";
 const std::string reallimits_type_name = "ModelView::RealLimits";
+
 
 } // namespace ModelView::Constants
 
