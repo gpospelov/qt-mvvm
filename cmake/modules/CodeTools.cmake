35cbd5e2cd99a6e0784e5339c7d7118544f48e2f
Gennady Pospelov <gennady.pospelov@iter.org>
2024-08-11 18:55:34 +0200

    Provide automatic gtest download as backup option

--- cmake/modules/CodeTools.cmake
+++ cmake/modules/CodeTools.cmake
@@ -46,6 +46,17 @@ function(project_testcoverage_setup)
         )
 endfunction()
 
+# Fetches google test version 1.12.1
+function(fetch_googletest)
+  include(FetchContent)
+  FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG 58d77fa8070e8cec2dc1ed015d66b454c8d78850)
+FetchContent_MakeAvailable(googletest)
+  message(VERBOSE "GTest binaries are present at ${googletest_BINARY_DIR}")
+endfunction()
+
 # Setups targets for code processing.
 
 function(project_codetools_setup)
