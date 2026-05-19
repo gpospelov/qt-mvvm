acafe74bf80f93a0793eb8fa510eb004f9f97e18
amaa-99 <amaa-99>
2026-05-19 18:10:23 +0000

    CI build fails due to googletest mismatch: Modified the CMake configuration to always fetch the googletest package instead of using whatever version is found in the system (workaround for build errors). Updated the fetched googletest version to the current release (v1.17.0).

--- cmake/modules/CodeTools.cmake
+++ cmake/modules/CodeTools.cmake
@@ -49,10 +49,8 @@ endfunction()
 # Fetches google test version 1.12.1
 function(fetch_googletest)
   include(FetchContent)
-  FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG 58d77fa8070e8cec2dc1ed015d66b454c8d78850)
+  FetchContent_Declare(googletest GIT_REPOSITORY https://github.com/google/googletest.git GIT_TAG v1.17.0)
+
 FetchContent_MakeAvailable(googletest)
   message(VERBOSE "GTest binaries are present at ${googletest_BINARY_DIR}")
 endfunction()
